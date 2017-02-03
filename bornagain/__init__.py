ee208bf19424fe9d998ed98b393f1bc7899fd209
Richard A. Kirian <rkirian@asu.edu>
2017-02-03 04:32:44 -0700

    Lysozyme simulation example in playground

--- bornagain/__init__.py
+++ bornagain/__init__.py
@@ -3,6 +3,6 @@ import detector
 import scatter
 import simulate
 import source
+import target
 import units
 import utils
-
--- playground/test-simulation.py
+++ playground/test-simulation.py
@@ -4,15 +4,20 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-import bornagain as dif
+import bornagain as ba
 import bornagain.simulate.clcore as clcore
+#import bornagain.structure.crystal as crystal
 
 
-pl = dif.detector.panelList()
-
+# Create a detector
+pl = ba.detector.panelList()
 pl.simpleSetup(1000,1000,100e-6,0.05,1.5e-10)
 
+# Scattering vectors
 Q = pl.Q
+
+# Load a crystal structure
+cryst = ba.target.crystal.structure('../examples/data/pdb/2LYZ.pdb')
 
 # r = np.zeros([3,3])
 # 
@@ -23,11 +28,16 @@ Q = pl.Q
 # r[2,0] = 6e-10
 # r[2,1] = 3e-10
 
-N = 10000
-r = np.random.normal(0,10e-10,[N,3])
+#N = 10000
+#r = np.random.normal(0,10e-10,[N,3])
 #r[:,2] = 0
 
-f = np.random.random([N]) + 1j*np.random.random([N])
+r = cryst.r.T.copy()
+
+f = ba.simulate.utils.atomicScatteringFactors(cryst.Z, pl.beam.wavelength)
+
+
+#f = np.random.random([N]) + 1j*np.random.random([N])
 #f = None
 
 t = time.time()
