5d854ce85b39be28e41aca285e9c2b874a1d286b
dermen <dermen@stanford.edu>
2018-03-26 21:22:21 -0700

    moved placer

--- bornagain/target/__init__.py
+++ bornagain/target/__init__.py
@@ -1,6 +1,6 @@
 from . import crystal
 from .crystal import Atoms, Molecule, Lattice
-
+from .placer import Place
 r"""
 The target package contains classes, functions, etc. that are related to the objects we shoot with x-rays.  Crystals,
 molecules, atoms, electron density maps, and so on.
