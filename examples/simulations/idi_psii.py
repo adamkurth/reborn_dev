dd01929307760b1ba68a3418c5bbb5bdc0ac0183
dermen <dermen@stanford.edu>
2018-03-26 21:18:58 -0700

    idi updates for box detector and direct SAXS computation, still need at check/add solid angles

--- examples/simulations/idi_psii.py
+++ examples/simulations/idi_psii.py
@@ -1,8 +1,12 @@
 from __future__ import division
 
+
+import warnings
+warnings.filterwarnings("ignore")
+
 import sys
 from time import time
-
+import pylab as plt
 import numpy as np
 np.seterr(divide='ignore', invalid='ignore') # We expect a divide-by-zero, which is corrected... I don't like the annoying message...
 from scipy.stats import binned_statistic_dd
@@ -13,7 +17,7 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
 
 sys.path.append("../..")
 import bornagain as ba
-from bornagain.target import crystal
+from bornagain.target import crystal, Place
 from bornagain.units import hc, keV
 from bornagain.simulate.clcore import ClCore
 import pyqtgraph.opengl as gl
@@ -23,7 +27,7 @@ import pyqtgraph as pg
 qtview = True
 
 # How many diffraction patterns to simulate
-n_patterns = 100
+n_patterns = 2000
 
 # Intensity of the fluoress
 add_noise = True
@@ -33,148 +37,49 @@ photons_per_atom = 100
 use_henke = True  # if False, then use Cromer-mann version
 
 # Information about the object
-n_molecules = 1
+n_molecules = 100
 box_size = 1000e-9
-do_rotations = False
+do_rotations = True #False
 do_phases = True
 do_translations = True
 
+
+# Settings for pixel-array detector
+n_pixels = 64
+pixel_size = 0.001 # meters, (small pixels that we will bin average later)
+detector_distance = .05 # meter
+#pix_size = 0.1 * sqrt( 2*rmax*rmax/(wavelength*wavelength) -1 ) / ( n_pixels/2.) # Im not sure if this works, but trying to guess the min pixel size needed for shannon sampling at the edge of the detector.. 
+
+# Settings for spherical detector
+spherical_detector = False
+n_subdivisions = 3
+radius = 1
+
+
+####################################
+
+
 # Information about the emission
-photon_energy = 6.5 / keV
-wavelength = hc / photon_energy
+photon_energy = 16.5 / keV
+wavelength = hc / photon_energy # in meters
 beam_vec = np.array([0, 0, 1.0]) # This shouldn't matter...
 
 # Atomic positions of Mn atoms:
 pdb_file = '../data/pdb/3wu2.pdb'
 cryst = crystal.structure(pdb_file)
-Z = cryst.Z
-i = np.where(Z == 25)
-Z = Z[i].ravel()
-r = cryst.r
-r = np.take(r, i, axis=0)
-# print(r.shape)
-r = np.squeeze(r)
-r = r[:4, :]
-# print(r)
-r = np.array([[0, 0, 0], [5e-10, 0, 0]])
+is_manga = cryst.Z==25
+r = cryst.r[ is_manga]
+r = r[:4] # take the first monomer in assymetric unit, 
 r -= r.mean(0)  # mean sub, I dunno it matters or not , but for rotations maybe...
-
-# Settings for pixel-array detector
-n_pixels = 100
-pixel_size = 0.0005
-detector_distance = .05
-
-# Settings for spherical detector
-spherical_detector = 1 #False# True
-n_subdivisions = 3
-radius = 1
-
-
-print('Will simulate %d patterns' % (n_patterns))
-
-# TODO: move this to detector?
-
-
-
-
-###########
-# TODO: move this into e.g. target
-#############
-
-class Place(cKDTree):
-    def __init__(self, box_edge, min_dist, max_try=10000, *args, **kwargs):
-        """
-        Place points into a box of edge length box_edge, and don't let any two points
-        
-        Parameters
-        ==========
-        get with    t0 = time()in min_dist from one another.
-
-        box_edge, float
-            side length of the box to place spheres into
-        min_dist, float
-            minimum distance between two points in the box
-        max_try, int
-            number of times to try placing a new point such 
-            that is does not overlap
-        
-        """
-        np.random.seed()
-        a = np.random.uniform(0, box_edge, (1, 3))
-        cKDTree.__init__(self, a, *args, **kwargs)
-        self.min_dist = min_dist
-        self.box_edge = box_edge
-        self.max_try = max_try
-        self.too_dense = False
-
-    def insert(self):
-        """adds a new point to the box"""
-        new_pt = np.random.uniform(0, self.box_edge, (1, 3))
-        n_try = 0
-        # @dermen - why is the inequality comparing with inf?
-        is_overlapping = self.query(new_pt, distance_upper_bound=self.min_dist)[
-                             0] < np.inf  # query for a nearest neighbor
-        while is_overlapping:
-            new_pt = np.random.uniform(0, self.box_edge, (1, 3))
-            is_overlapping = self.query(new_pt, distance_upper_bound=self.min_dist)[0] < np.inf
-            n_try += 1
-            if n_try > self.max_try:
-                print("Getting too tight in here!")
-                self.too_dense = True
-                return
-        data = np.concatenate((self.data, new_pt))  # combine new pt and old pts
-        super(Place, self).__init__(data)  # re-initialize the parent class with new data
-
-
-
-
-# def place_spheres(Vf, sph_rad=1., box_edge=None, Nspheres=1000, tol=0.01):
-#     """
-#     Vf, float
-#         Fraction of sample volume occupied by spheres
-#     Nspheres, int
-#         how many spheres in the sample volume
-#     tol, float
-#         minimum distance the unit spheres can be to one another
-#     """
-#     #   volume of a unit sphere
-#     sph_vol = (4 / 3.) * np.pi * (sph_rad) ** 3
-#
-#     if box_edge is not None:
-#         #       then we let Nspheres be a free
-#         box_vol = box_edge ** 3
-#         Nspheres = int((box_vol * Vf) / sph_vol)
-#     else:
-#         #       then Nspheres determines the size of the box
-#         box_vol = sph_vol * Nspheres / Vf
-#         box_edge = np.power(box_vol, 1 / 3.)
-#
-#     min_dist = 2 * sph_rad + tol  # diameter plus tol,
-#
-#     print("Placing %d spheres into a box of side length %.4f" % (Nspheres, box_edge))
-#
-#     p = Place(box_edge, min_dist)  # init the Placer
-#     while p.n < Nspheres:
-#         p.insert()  # insert pt!
-#         if p.too_dense:
-#             print("\tbreaking insert loop with %d/%d spheres" % (p.n, Nspheres))
-#             break
-#
-#     return p.data
-
-
-###########
-# END COPY/PASTE
-###########
-
+n_atoms = r.shape[0]
 
 # maximum distance spanned by the molecule:
 r_size = distance.pdist(r).max()
-n_atoms = r.shape[0]
 
-
+print('Will simulate %d patterns' % (n_patterns))
 
 if spherical_detector:
+    detect_type = "SPHERE"
     print('Creating spherical detector...')
     ico = ba.detector.IcosphereGeometry(n_subdivisions=n_subdivisions, radius=radius)
     verts, faces, fcs = ico.compute_vertices_and_faces()
@@ -182,26 +87,63 @@ if spherical_detector:
     q = (2 * np.pi / wavelength) * fcs #(fcs - beam_vec)
     print('%d pixels' % (q.shape[0]))
 else:
-    pad = ba.detector.PADGeometry()
-    pad.simple_setup(n_pixels=n_pixels, pixel_size=pixel_size, distance=detector_distance)
-    q = pad.q_vecs(beam_vec=beam_vec, wavelength=wavelength)
-
+    detect_type='BOX'
+    n = n_pixels # shortcut
+    p = pixel_size # shortcut
+    
+    pad1 = ba.detector.PADGeometry()
+    pad1.n_fs = n
+    pad1.n_ss = n
+    pad1.fs_vec = [0,0,p]
+    pad1.ss_vec = [0,p,0]
+    pad1.t_vec = [ -detector_distance,  (-n*p+p)*.5 , (-n*p+p)*.5  ]
+    #pad.simple_setup(n_pixels=n_pixels, pixel_size=pixel_size, distance=detector_distance)
+    pad2 = ba.detector.PADGeometry()
+    pad2.n_fs = n
+    pad2.n_ss = n
+    pad2.fs_vec = [0,0,p]
+    pad2.ss_vec = [0,p,0]
+    pad2.t_vec = [ detector_distance,  (-n*p+p)*.5 , (-n*p+p)*.5  ]
+    #pad.simple_setup(n_pixels=n_pixels, pixel_size=pixel_size, distance=detector_distance)
+    
+    pad3 = ba.detector.PADGeometry()
+    pad3.n_fs = n
+    pad3.n_ss = n
+    pad3.fs_vec = [p,0,0]
+    pad3.ss_vec = [0,p,0]
+    pad3.t_vec = [ (-n*p+p)*.5 , (-n*p+p)*.5, detector_distance]
+    
+    #pad.simple_setup(n_pixels=n_pixels, pixel_size=pixel_size, distance=detector_distance)
+    
+    q1 = pad1.q_vecs(beam_vec=beam_vec, wavelength=wavelength)
+    q2 = pad2.q_vecs(beam_vec=beam_vec, wavelength=wavelength)
+    q3 = pad3.q_vecs(beam_vec=beam_vec, wavelength=wavelength)
+    pad1_sh= pad1.shape()
+    pad2_sh= pad2.shape()
+    pad3_sh= pad3.shape()
+    print ("Making 3 PADS each with shape %d x %d"% ( pad1_sh[0], pad1_sh[1] ))
+    #   combine the qs into a single vector...
+    q = np.vstack( ( q1,q2,q3) )
+    k_vecs =np.vstack( (pad1.position_vecs(), pad2.position_vecs(), pad3.position_vecs()) )
+    k_vecs *= 2 * np.pi / wavelength
+    q12 = distance.cdist( k_vecs, k_vecs).ravel() # pair q distances
+    nbins=512 # number of q bins
+    qbins = np.linspace( q12.min(), q12.max(), nbins+1) # these are the histogram bins.. 
+    print("The pads cover the range %.4f to %.4f inverse angstrom"%(q12.min()*1e-10, q12.max()*1e-10))
+    plt.hist( qbins*1e-10, bins=qbins*1e-10)
+    plt.xlabel("inverse angstrom")
+    plt.ylabel("bin count")
+    plt.show()
 n_q = q.shape[0]
 
-# # Place molecules in a 10 nm box
-# dimer_placer = Place(box_edge=10e-9, min_dist=r_size)
-# for i in range(n_molecules):
-#     dimer_placer.insert()
-# dimer_pos = dimer_placer.data
+print("Simulating intensities for %d pixels in the %s detector.." %(n_q, detect_type))
+I_sum = np.zeros( n_q, dtype=np.float64) # store the intensities
+II_sum = np.zeros( n_q*n_q, dtype=np.float64)  # stores the correlations of intensities
 
-print('Simulating intensities...')
-I_sum = 0
-II_sum = 0
 clcore = ClCore(group_size=1)
 q_dev = clcore.to_device(q)
 seconds = 0
-t0 = time()
-t = time()
+t0 = t=  time()
 for pattern_num in range(0, n_patterns):
 
     # Random phases for each atom
@@ -218,8 +160,8 @@ for pattern_num in range(0, n_patterns):
 #       insert all the molecules...
         
         for i_n in range( n_molecules):
-            if i_n %100 == 0:
-                print("placing %d / %d molecules in box" % ( i_n+1, n_molecules ) )
+            #if i_n %100 == 0:
+            #    print("placing %d / %d molecules in box" % ( i_n+1, n_molecules ) )
             placer.insert()
         Ts = placer.data # this is same units as r_size and box_size...
     else:
@@ -251,75 +193,57 @@ for pattern_num in range(0, n_patterns):
     I *= photons_per_atom * n_atoms * n_molecules / np.sum(I.ravel())
     if add_noise: I = np.random.poisson(I)
 
-    I_sum += I
-    II_sum += np.multiply.outer(I, I).ravel()
+    I_sum += I # summing the intensities
+
+    #II_sum += np.multiply.outer(I, I).ravel() # here is summing the correlations of intensities.. 
+    print("computing correlation")
+    II_sum += np.einsum( 'i,j->ij', I,I).ravel()
+#    or 
+    #II_sum += (I[:,None]*I[None,:] ).ravel()
+#   prob best to do this on te GPU, it might be the bottleneck.. 
 
 print('Post-processing...')
 
-# I_sum /= n_patterns
-# II_sum /= n_patterns
-n_q = q.shape[0]
+if not spherical_detector:
+#   make magnitude of k1-k2 vectors for binning
+    print("computing distance matrix for all pairs of k1,k2")
+    qbin_count = np.histogram( q12, bins=qbins )[0]
+    qbin_sums = np.histogram( q12, bins=qbins , weights=II_sum)[0]
+    plt.plot( 1e-10* ( qbins[:-1]*.5 + qbins[1:]*.5 ), qbin_sums / qbin_count) 
+    plt.show()
+
 if spherical_detector:
-    print fcs.shape, n_q
-    qq = [np.subtract.outer(fcs[:, i], fcs[:, i]).ravel() for i in range(0, 3)]
+#   can use braodcasting here:
+    #qq = [np.subtract.outer(fcs[:, i], fcs[:, i]).ravel() for i in range(0, 3)]
+    #qq = 2*np.pi/wavelength*np.ravel(qq).reshape([3, n_q**2]).T.copy()
+    
+    qq = np.vstack( fcs[:,None] - fcs[None,:] ) * 2 * pi / wavelength
+    q_mags = np.sqrt(np.sum(q**2, axis=1))
+    max_q = 4*np.pi/wavelength
 
-    #print qq
-    #print qq.shape
-exit()
-qq = 2*np.pi/wavelength*np.ravel(qq).reshape([3, n_q**2]).T.copy()
-q_mags = np.sqrt(np.sum(q**2, axis=1))
-max_q = 4*np.pi/wavelength
+    n_bins_3d = 51
+    III, _, _ = binned_statistic_dd(sample=qq, values=II_sum, bins=(n_bins_3d,)*3, range=[(-max_q, max_q)]*3, statistic='sum')
+    IIIc, _, _ = binned_statistic_dd(sample=qq, values=np.ones(II_sum.shape), bins=(n_bins_3d,)*3, range=[(-max_q, max_q)]*3, statistic='sum')
+    mean_ = np.mean(I_sum/n_patterns)
+    III = (III/IIIc - (mean_)**2)/mean_**2
+    c = np.floor(n_bins_3d/2).astype(np.int)
+    min_ = np.min(III[np.isfinite(III)])
+    III[~np.isfinite(III)] = min_
+    III[c, c, c] = min_
 
-n_bins_3d = 51
-III, _, _ = binned_statistic_dd(sample=qq, values=II_sum, bins=(n_bins_3d,)*3, range=[(-max_q, max_q)]*3, statistic='sum')
-IIIc, _, _ = binned_statistic_dd(sample=qq, values=np.ones(II_sum.shape), bins=(n_bins_3d,)*3, range=[(-max_q, max_q)]*3, statistic='sum')
-mean_ = np.mean(I_sum/n_patterns)
-III = (III/IIIc - (mean_)**2)/mean_**2
-c = np.floor(n_bins_3d/2).astype(np.int)
-min_ = np.min(III[np.isfinite(III)])
-III[~np.isfinite(III)] = min_
-print(c, min_)
-III[c, c, c] = min_
+    if qtview:
+        print('Displaying results...')
 
-if qtview:
-    print('Displaying results...')
+        im = pg.image(np.transpose(III, axes=(1, 0, 2)))
+        im.setCurrentIndex(np.floor(n_bins_3d/2).astype(np.int))
 
-    im = pg.image(np.transpose(III, axes=(1, 0, 2)))
-    im.setCurrentIndex(np.floor(n_bins_3d/2).astype(np.int))
+        qa = pg.mkQApp()
+        face_colors = np.ones([n_faces, 4])
+        for i in range(0,3): face_colors[:, i] = (I / np.max(I))*0.95 + 0.05
+        vw = gl.GLViewWidget()
+        vw.show()
+        md = gl.MeshData(vertexes=verts, faces=faces, faceColors=face_colors)
+        mi = gl.GLMeshItem(meshdata=md, smooth=False) #, edgeColor=np.array([0.1, 0.1, 0.1])*255, drawEdges=True)
+        vw.addItem(mi)
+        qa.exec_()
 
-    qa = pg.mkQApp()
-    face_colors = np.ones([n_faces, 4])
-    for i in range(0,3): face_colors[:, i] = (I / np.max(I))*0.95 + 0.05
-    vw = gl.GLViewWidget()
-    vw.show()
-    md = gl.MeshData(vertexes=verts, faces=faces, faceColors=face_colors)
-    mi = gl.GLMeshItem(meshdata=md, smooth=False) #, edgeColor=np.array([0.1, 0.1, 0.1])*255, drawEdges=True)
-    vw.addItem(mi)
-    qa.exec_()
-
-# glw = pg.GraphicsLayoutWidget()
-# glw.addItem(vw)
-
-
-# if spherical_detector:
-#     fig = plt.figure()
-#     ax = fig.add_subplot(111, projection='3d')
-#     mesh = Poly3DCollection(verts[faces], facecolors=mpl.cm.gray(I_noisy / np.max(I_noisy)), alpha=1, linewidths=0)
-#     ax.add_collection3d(mesh, )
-#     ax.scatter(fcs[:, 0], fcs[:, 1], fcs[:, 2], c='g', s=0, lw=0, alpha=0)
-#     ax.set_aspect('equal', adjustable='box')
-#     plt.show()
-# else:
-#     imdisp = I_noisy.reshape(pad.shape())
-#     plt.imshow(np.log10(imdisp + 1), interpolation='nearest', cmap='gray')
-#     plt.show()
-
-
-    # plt.figure().add_subplot(111, projection='3d').scatter(v[:,0], v[:,1], v[:,2])
-    # plt.figure().add_subplot(111, projection='3d').plot_surface(v[:,0], v[:,1], v[:,2])
-    # plt.show()
-
-
-    # Questions:
-    # How does SNR scale with number of molecules?
-    # How many shots needed?
