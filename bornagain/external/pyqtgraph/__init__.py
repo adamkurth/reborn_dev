b78bc4656cca10eb79b6f9b98b34959e52529c27
Richard A. Kirian <rkirian@asu.edu>
2018-09-04 23:08:16 -0700

    gitignore and pyqtgraph doc

--- bornagain/external/pyqtgraph/__init__.py
+++ bornagain/external/pyqtgraph/__init__.py
@@ -1,7 +1,13 @@
 """
 This contains the entire pyqtgraph module along with a few extra functions. 
 
-keep_open    Utility for keeping window open
+The pyqtgraph module was cloned from here:
+https://github.com/pyqtgraph/pyqtgraph-core
+
+
+Here are the extra utilities:
+
+keep_open    Keeps a window open (e.g. when window vanishes upon exiting script)
 
 Utilities for operating on the colormap of multiple images at once
 MultiHistogramLUTWidget
