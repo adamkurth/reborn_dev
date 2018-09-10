a39cc391580a90335fd7652941df74c3c3d2c05a
Richard A. Kirian <rkirian@asu.edu>
2018-09-09 23:20:14 -0700

    Documentation

--- bornagain/external/pyqtgraph/__init__.py
+++ bornagain/external/pyqtgraph/__init__.py
@@ -1,20 +1,5 @@
 """
-This contains the entire pyqtgraph module along with a few extra functions. 
-
-The pyqtgraph module was cloned from here:
-https://github.com/pyqtgraph/pyqtgraph-core
-
-
-Here are the extra utilities:
-
-keep_open    Keeps a window open (e.g. when window vanishes upon exiting script)
-
-Utilities for operating on the colormap of multiple images at once
-MultiHistogramLUTWidget
-MultiHistogramLUTItem
-
-Here are the bugfixes:
-ImageItem - got rid of the flicker due to undersampling of the image when zoomed out.
+This contains some extra functions for working with `pyqtgraph <http://pyqtgraph.org/>`_.
 """
 
 from .extras import *
