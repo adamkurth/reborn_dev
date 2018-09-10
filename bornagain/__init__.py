9e073e1c1b564181820aee0dbf904fe98ceb281f
Richard A. Kirian <rkirian@asu.edu>
2018-09-09 21:53:37 -0700

    Added global config options and set_global/get_global functions

--- bornagain/__init__.py
+++ bornagain/__init__.py
@@ -8,3 +8,22 @@ from . import utils
 
 from .detector import SimplePAD
 from .target.crystal import Molecule
+
+
+CONFIG_OPTIONS = {'warn_depreciated': True,
+                  'force_depreciated': False}
+
+
+def set_global(opt, value):
+    global CONFIG_OPTIONS
+    if opt not in CONFIG_OPTIONS:
+        raise KeyError('Unknown configuration option "%s"' % opt)
+    if opt == 'warn_depreciated' and value not in (True, False):
+        raise ValueError('warn_depreciated must be either True or False')
+    CONFIG_OPTIONS[opt] = value
+
+
+def get_global(opt):
+    """Return the value of a single global configuration option.
+    """
+    return CONFIG_OPTIONS[opt]
