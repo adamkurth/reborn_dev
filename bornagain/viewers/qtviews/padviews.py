9fae91d9bd6eb9fb320ae4089b47478c418cc1dc
Richard A. Kirian <rkirian@asu.edu>
2018-09-09 19:52:46 -0700

    padview example

--- bornagain/viewers/qtviews/padviews.py
+++ bornagain/viewers/qtviews/padviews.py
@@ -1,98 +1,288 @@
 import numpy as np
 from PyQt5 import uic
+from PyQt5.QtWidgets import QInputDialog, QLineEdit
+from PyQt5.QtGui import QShortcut, QKeySequence, QTransform
 import pkg_resources
 
 import pyqtgraph as pg
+pg.setConfigOptions(imageAxisOrder='row-major')
 import bornagain.external.pyqtgraph as bpg
+from bornagain.utils import vec_norm, vec_mag
+import bornagain as ba
 
 padviewui = pkg_resources.resource_filename('bornagain.viewers.qtviews', 'padview.ui')
 
 class PADView(object):
 
+
+    r"""
+    This is supposed to be an easy way to view PAD data, provided a list of PADGeometry instances.
+
+    It is a work in progress...
+    """
+
+    # Note that most of the interface was created using the QT Designer tool.  Here are some important ones:
+    # self.histogram is a bornagain.external.pyqtgraph.MultiHistogramLUTWidget
+
     pad_geometry = []
-    data = []
+    pad_data = []
+    pad_labels = None
     rois = []
     images = []
     rings = []
     grid = None
+    coord_axes = None
 
-    def __init__(self, pad_geometry=[], data=[]):
+    def __init__(self, pad_geometry=[], pad_data=[], logscale=False):
 
         self.pad_geometry = pad_geometry
-        self.data = data
+        self.pad_data = pad_data
+        self.logscale = logscale
 
         self.app = pg.mkQApp()
         self.main_window = uic.loadUi(padviewui)
         self.viewbox = pg.ViewBox()
+        self.viewbox.invertX()
         self.viewbox.setAspectLocked()
         self.main_window.graphics_view.setCentralItem(self.viewbox)
         self.setup_pads()
         self.setup_histogram_tool()
         self.main_window.show()
+        self.proxy = pg.SignalProxy(self.viewbox.scene().sigMouseMoved, rateLimit=60, slot=self.mouse_moved)
+        # self.label = pg.LabelItem(justify='right')
+        # self.viewbox.addItem(self.label)
+        self.main_window.actionGrid.triggered.connect(self.toggle_grid)
+        self.main_window.actionRings.triggered.connect(self.edit_ring_radii)
+
+        self.grid_shortcut = QShortcut(QKeySequence("Ctrl+g"), self.main_window)
+        self.grid_shortcut.activated.connect(self.toggle_grid)
+
+        self.rings_shortcut = QShortcut(QKeySequence("Ctrl+r"), self.main_window)
+        self.rings_shortcut.activated.connect(self.edit_ring_radii)
+
+        self.coords_shortcut = QShortcut(QKeySequence("Ctrl+a"), self.main_window)
+        self.coords_shortcut.activated.connect(self.toggle_coordinate_axes)
+
+        self.label_shortcut = QShortcut(QKeySequence("Ctrl+l"), self.main_window)
+        self.label_shortcut.activated.connect(self.toggle_pad_labels)
+
+
         # self.main_window.setWindowState(self.main_window.windowState()
         #                                 & ~pg.QtCore.Qt.WindowMinimized
         #                                 | pg.QtCore.Qt.WindowActive)
         # self.main_window.activateWindow()
         # self.main_window.showMaximized()
 
+    @property
+    def n_pads(self):
+
+        return len(self.pad_geometry)
+
     def setup_histogram_tool(self):
 
+        self.main_window.histogram.gradient.loadPreset('flame')
         self.main_window.histogram.setImageItems(self.images)
-        # self.lut = bpg.MultiHistogramLUTWidget()
-        # self.viewbox2 = pg.ViewBox()
-        # self.main_window.graphics_view2.setCentralItem(self.lut)
 
-    def setup_pads(self):
+    def show_coordinate_axes(self):
 
-        self.overall_levels = np.array([np.min(np.ravel(self.data)), np.max(np.ravel(self.data))])
+        if self.coord_axes is None:
+            L = 100
+            x = pg.PlotDataItem(np.array([0, 1])*L, np.array([0, 0])*L, pen=pg.mkPen('r'), width=5)
+            y = pg.PlotDataItem(np.array([0, 0]) * L, np.array([0, 1]) * L, pen=pg.mkPen('g'), width=5)
+            z = pg.ScatterPlotItem([0], [0], pen=None, brush=pg.mkBrush('b'))
+            self.coord_axes = [x, y, z]
+            self.viewbox.addItem(z)
+            self.viewbox.addItem(x)
+            self.viewbox.addItem(y)
 
-        for i in range(0, len(self.pad_geometry)):
-            p = self.pad_geometry[i]
-            d = self.data[i]
-            # d[0:10, 0:100] = np.max(d)
-            s = d.shape
-            t = p.t_vec.flat[0:2]/p.pixel_size()
-            t[1] *= -1
-            r = pg.ROI(t, s)
-            r.translatable = False
-            r.rotateAllowed = False
-            r.setPen(None)
-            im = bpg.ImageItem(d) #, levels=self.overall_levels)
-            im.setAutoDownsample(2)
-            im.setParentItem(r)
-            self.viewbox.addItem(r)
-            self.rois.append(r)
-            self.images.append(im)
+    def hide_coordinate_axes(self):
 
-    def toggle_geometry_adjustment(self):
+        if self.coord_axes is not None:
+            for c in self.coord_axes:
+                self.viewbox.removeItem(c)
+            self.coord_axes = None
 
-        if roi.translatable:
-            self.disable_geometry_adjustment()
+    def toggle_coordinate_axes(self):
+
+        if self.coord_axes is None:
+            self.show_coordinate_axes()
         else:
-            self.enable_geometry_adjustment()
+            self.hide_coordinate_axes()
 
-    def enable_geometry_adjustment(self):
+    def show_pad_labels(self):
 
-        for roi in self.rois:
+        if self.pad_labels is None:
 
-            roi.translatable = True
-            roi.rotateAllowed = True
-            roi.setPen(pg.mkPen('g'), width=4)
-            roi.addRotateHandle([0, 0], [0.5, 0.5])
-            roi.addRotateHandle([0, .5], [0.5, 0.5])
+            self.pad_labels = []
 
-        self.add_grid()
+            for i in range(0, self.n_pads):
 
-    def disable_geometry_adjustment(self):
+                lab = pg.TextItem(text="%d" % i, fill=pg.mkBrush('b'), anchor=(0.5, 0.5))
+                g = self.pad_geometry[i]
+                fs = g.fs_vec.ravel()*g.n_fs/2
+                ss = g.ss_vec.ravel()*g.n_ss/2
+                t = g.t_vec.ravel()
+                x = (fs[0] + ss[0] + t[0])/g.pixel_size()
+                y = (fs[1] + ss[1] + t[1])/g.pixel_size()
+                lab.setPos(x, y)
+                self.pad_labels.append(lab)
+                self.viewbox.addItem(lab)
 
-        for roi in self.rois:
+    def hide_pad_labels(self):
 
-            roi.translatable = False
-            roi.rotateAllowed = False
-            roi.setPen(None)
-            for handle in roi.handles:
-                roi.removeHandle(handle)
-            self.remove_grid()
+        if self.pad_labels is not None:
+            for lab in self.pad_labels:
+                self.viewbox.removeItem(lab)
+            self.pad_labels = None
+
+    def toggle_pad_labels(self):
+
+        if self.pad_labels is None:
+            self.show_pad_labels()
+        else:
+            self.hide_pad_labels()
+
+    def _apply_pad_transform(self, im, p):
+
+        # This is really aweful.  I don't know if this is the best way to do the transforms, but it's the best
+        # I could do given the fact that I'm not able to track down all the needed info on how all of these
+        # Transforms are applied.  I can only say that a physicist did not invent this system.
+
+        # 3D basis vectors of panel (length encodes pixel size):
+        f = p.fs_vec.ravel()
+        s = p.ss_vec.ravel()
+
+        # 3D translation to *center* of corner pixel (first pixel in memory):
+        t = p.t_vec.ravel()
+
+        # Normalize all vectors to pixel size.  This is a hack that needs to be fixed later.  Obviously, we cannot
+        # show multiple detectors at different distances using this stupid pixel-based convention.
+        ps = p.pixel_size()
+        f /= ps
+        s /= ps
+        t /= ps
+
+        # Strip off the z component.  Another dumb move.
+        f = f[0:2]
+        s = s[0:2]
+        t = t[0:2]
+
+        # Offset translation since pixel should be centered.
+        t -= np.array([0.5, 0.5])
+
+        # These two operations set the "home" position of the panel such that the fast-scan direction
+        # is along the viewbox "x" axis.  I don't know if this is the corret thing to do -- needs further
+        # attention.  I would imagine that pyqtgraph has a better way to do this, but I haven't found the right
+        # feature yet.
+        im.scale(1, -1)
+        im.rotate(-90)
+
+        # Scale the axes.  This is a phony way to deal with detector tilts.  Better than nothing I guess.
+        scf = np.sqrt(np.sum(f * f))
+        scs = np.sqrt(np.sum(s * s))
+
+        # Note that the *sign* of this scale factor takes care of the fact that 2D rotations alone
+        # cannot deal with a transpose operation.  What follows is a confusing hack... must be a better way.
+        sign = np.sign(np.cross(np.array([f[0], f[1], 0]), np.array([s[0], s[1], 0]))[2])
+
+        # Here goes the re-scaling
+        im.scale(sign * scs, scf)
+
+        # Rotate the axes
+        fnorm = f / np.sqrt(np.sum(f * f))
+        ang = np.arctan2(fnorm[1], fnorm[0])
+        # No, wait, don't rotate the axes... experimentation says to translate first despite the fact that it
+        # doesn't make much sense to do things that way.  Maybe I just gave up too soon, but I found that I needed
+        # to translate first...
+
+        # Translate the scaled/rotated image.  Turns out we need to flip the sign of the translation vector
+        # coordinate corresponding to the axis we flipped.  I don't know why, but I've completely given up on the
+        # idea of understanding the many layers of coordinate systems and transformations...
+        im.translate(sign * t[1], t[0])
+        im.rotate(-sign * ang * 180 / np.pi)
+
+        # Now, one would *think* that we could define a simple matrix transform, and then translate the resulting
+        # image (which has been rotated and possibly skewed to imitate a 3D rotation).  I tried and failed.  But
+        # It's worthwile to try again, since it would be brilliant if we could just learn how to define these
+        # transforms and apply them to all relevant graphics items.
+        # trans = QTransform()
+        # trans.scale(M[0, 0], M[1, 1])
+        # trans.translate(t[0], t[1])
+        # im.setTransform(trans)
+
+
+    def setup_pads(self, show_scans=False):
+
+        for i in range(0, self.n_pads):
+
+            # p = self.pad_geometry[i]
+            d = self.pad_data[i]
+
+            if self.logscale:
+                d[d < 0] = 0
+                d = np.log10(d)
+
+            if show_scans:  # For testing - show fast scan axis
+                d[0:np.ceil(d.shape[0]*0.005).astype(np.int),
+                  0:np.ceil(d.shape[1]*0.1).astype(np.int)] *= 0.1
+
+            im = bpg.ImageItem(d)
+
+            self._apply_pad_transform(im, self.pad_geometry[i])
+
+            self.images.append(im)
+            self.viewbox.addItem(im)
+
+    def mouse_moved(self, evt):
+
+        pos = evt[0]
+        pid = -1
+        ppos = (-1, -1)
+        intensity = -1
+        if self.viewbox.sceneBoundingRect().contains(pos):
+            for i in range(0, len(self.images)):
+                if self.images[i].sceneBoundingRect().contains(pos):
+                    pid = i
+                    ppos = self.images[i].mapFromScene(pos)
+                    ppos = (np.floor(ppos.x()), np.floor(ppos.y()))
+                    continue
+
+            # pnt = self.viewbox.mapSceneToView(pos)
+            fs = np.int(ppos[1])
+            ss = np.int(ppos[0])
+            if pid >= 0:
+                d = self.pad_data[pid]
+                sh = d.shape
+                if ss < sh[0] and fs < sh[1]:
+                    intensity = self.pad_data[pid][ss, fs]
+
+            if pid >= 0:
+                status = 'Panel %2d; ss=%4d; fs=%4d; I=%8g' % (pid, ss, fs, intensity)
+            else:
+                status = ''
+            self.main_window.statusbar.showMessage(status)
+            #self.label.setText(
+            #    "<span style='font-size: 12pt'>x=%0.1f,   <span style='color: red'>y1=%0.1f</span>" % (
+            #    pnt.x(), pnt.y()))
+
+    def edit_ring_radii(self):
+
+        # cur_rad = ""
+
+        text, ok = QInputDialog.getText(self.main_window, "Enter ring radii (comma separated)", "Ring radii", QLineEdit.Normal, "100,200")
+        if ok:
+            if text == '':
+                self.remove_rings()
+                return
+            r = text.split(',')
+            rad = []
+            for i in range(0, len(r)):
+                try:
+                    rad.append(float(r[i].strip()))
+                except:
+                    pass
+            self.remove_rings()
+            self.add_rings(rad)
 
     def add_rings(self, radii=[], pens=None, radius_handle=False):
 
@@ -102,7 +292,7 @@ class PADView(object):
         n = len(radii)
 
         if pens is None:
-            pens = [pg.glColor([255, 255, 255])]*n
+            pens = [pg.mkPen([255, 255, 255], width=2)]*n
 
         for i in range(0, n):
             circ = pg.CircleROI(pos=[-radii[i]*0.5]*2, size=radii[i], pen=pens[i])
@@ -119,13 +309,16 @@ class PADView(object):
 
         for circ in self.rings:
             for handle in circ.handles:
-                print(circ)
-                print(handle)
                 circ.removeHandle(handle['item'])
 
     def remove_rings(self):
 
-        pass
+        if self.rings is None:
+            return
+
+        for i in range(0, len(self.rings)):
+            self.viewbox.removeItem(self.rings[i])
+
 
     def add_grid(self):
 
@@ -137,12 +330,15 @@ class PADView(object):
 
         if self.grid is not None:
             self.viewbox.removeItem(self.grid)
+            self.grid = None
 
-    def init_ui(self):
+    def toggle_grid(self):
 
-        mw = self.main_window
-        mw.statusBar()
-        mw.center()
+        print('toggle grid')
+        if self.grid is None:
+            self.add_grid()
+        else:
+            self.remove_grid()
 
     def start(self):
 
--- examples/view/padview.py
+++ examples/view/padview.py
@@ -3,13 +3,75 @@ sys.path.append('../..')
 
 from bornagain.simulate import examples
 from bornagain.viewers.qtviews import PADView
+from bornagain import detector
 import numpy as np
-import pyqtgraph as pg
+# import pyqtgraph as pg
 
-sim = examples.lysozyme_molecule()
 
-I = [np.log10(d*0.001+10) for d in sim['intensity']]
-padgui = PADView(data=I, pad_geometry=sim['pad_geometry'])
+pads = []
+
+pad = detector.PADGeometry()
+pad.simple_setup(pixel_size=100e-6, distance=0.1)
+pads.append(pad)
+
+pad = detector.PADGeometry()
+pad.simple_setup(pixel_size=100e-6, distance=0.1)
+ang = 20*np.pi/180.
+R = np.array([[np.cos(ang), np.sin(ang), 0],[-np.sin(ang), np.cos(ang), 0],[0, 0, 1]])
+f = pad.fs_vec.copy()
+s = pad.ss_vec.copy()
+pad.fs_vec = np.dot(f, R)
+pad.ss_vec = np.dot(s, R)
+t = pad.t_vec.copy().ravel()
+t[0] += 0.04
+t[1] += 0.08
+pad.t_vec = t
+pads.append(pad)
+
+if True:
+    pad = detector.PADGeometry()
+    pad.simple_setup(pixel_size=100e-6, distance=0.1)
+    ang = 30 * np.pi / 180.
+    R = np.array([[np.cos(ang), np.sin(ang), 0], [-np.sin(ang), np.cos(ang), 0], [0, 0, 1]])
+    f = pad.fs_vec.copy()
+    s = pad.ss_vec.copy()
+    f = np.dot(f, R).ravel()
+    s = np.dot(s, R).ravel()
+    f[1] *= -1
+    s[1] *= -1
+    pad.fs_vec = f
+    pad.ss_vec = s
+    t = pad.t_vec.copy().ravel()
+    t[0] += 0.04
+    t[1] += 0.08
+    t[1] *= -1
+    pad.t_vec = t
+    pads.append(pad)
+
+if 0:
+    if 1:
+        pads = pads[0:2]
+    else:
+        pads = pads[1:]
+
+pads = examples.cspad_pads()
+# pads = examples.pnccd_pads()
+
+
+
+
+
+sim = examples.lysozyme_molecule(pads=pads)
+
+geom = sim['pad_geometry']
+I = sim['intensity']
+# I = [np.random.poisson(d*1e-5) + 1 for d in I]
+# I = [np.log10(d) for d in I]
+padgui = PADView(pad_data=I, pad_geometry=geom, logscale=True)
+
+# padgui.show_coordinate_axes()
+# padgui.add_grid()
+# padgui.show_pad_labels()
 # padgui.add_rings([200, 400, 600, 800], pens=[pg.mkPen([255, 0, 0], width=2)]*4)
-# padgui.enable_geometry_adjustment()
+
 padgui.start()
