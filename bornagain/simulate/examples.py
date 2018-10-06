f77f0e1d636d29d15271f4f448e4a8804e66fca3
Richard A. Kirian <rkirian@asu.edu>
2018-10-05 22:41:19 -0700

    Added simple class for generating single-molecule simulations.  Only for testing purposes - will likely be removed soon...

--- bornagain/simulate/examples.py
+++ bornagain/simulate/examples.py
@@ -1,7 +1,17 @@
+"""
+
+Some simple examples for testing purposes.
+
+Don't build any of this into your code.
+
+"""
+
 import pkg_resources
 import numpy as np
+import bornagain as ba
 from bornagain import detector
 from bornagain.units import hc
+from bornagain import utils
 from bornagain.external import crystfel
 from bornagain.simulate import atoms
 from bornagain.target.crystal import Structure
@@ -41,7 +51,7 @@ def cspad_pads():
     return crystfel.geometry_file_to_pad_geometry_list(cspad_geom_file)
 
 
-def lysozyme_molecule(pads=None, wavelength=None):
+def lysozyme_molecule(pads=None, wavelength=None, random_rotation=False):
 
     r"""
 
@@ -71,9 +81,86 @@ def lysozyme_molecule(pads=None, wavelength=None):
     q = [pad.q_vecs(beam_vec=[0, 0, 1], wavelength=wavelength) for pad in pads]
     q = np.ravel(q)
 
-    A = sim.phase_factor_qrf(q, r, f)
+
+    if random_rotation:
+        R = utils.random_rotation()
+    else:
+        R = None
+
+    A = sim.phase_factor_qrf(q, r, f, R)
     I = np.abs(A)**2
 
     data_list = detector.split_pad_data(pads, I)
 
     return {'pad_geometry': pads, 'intensity': data_list}
+
+
+class PDBMoleculeSimulator(object):
+
+    r"""
+
+    A simple generator of simulated single-molecule intensities, from a pdb file.  Defaults to lysozyme at 1.5 A
+    wavelength, on a pnccd detector layout.
+
+    """
+
+    def __init__(self, pdb_file=None, pads=None, wavelength=None, random_rotation=False):
+
+        r"""
+
+        This will setup the opencl simulation core.
+
+        Args:
+            pdb_file: path to a pdb file
+            pads: array of :class:`PADGeometry bornagain.detector.PADGeometry` intances
+            wavelength: in SI units of course
+            random_rotation: True or False
+        """
+
+        if pdb_file is None:
+            pdb_file = lysozyme_pdb_file
+
+        if pads is None:
+            pads = crystfel.geometry_file_to_pad_geometry_list(pnccd_geom_file)
+
+        if wavelength is None:
+            wavelength = 1.5e-10
+
+        photon_energy = hc / wavelength
+
+        self.clcore = ClCore(group_size=32, double_precision=False)
+        cryst = Structure(pdb_file)
+
+        self.random_rotation = random_rotation
+
+        r = cryst.r
+        f = atoms.get_scattering_factors(cryst.Z, photon_energy=photon_energy)
+        q = [pad.q_vecs(beam_vec=[0, 0, 1], wavelength=wavelength) for pad in pads]
+        q = np.ravel(q)
+
+        self.q_gpu = self.clcore.to_device(q)
+        self.r_gpu = self.clcore.to_device(r)
+        self.f_gpu = self.clcore.to_device(f)
+        self.a_gpu = self.clcore.to_device(shape=(self.q_gpu.shape[0]), dtype=self.clcore.complex_t)
+
+    def next(self):
+
+        r"""
+
+        Generate another simulated pattern.  No scaling or noise added.
+
+        Returns: Flat array of diffraction intensities.
+
+        """
+
+        if self.random_rotation:
+            R = utils.random_rotation()
+        else:
+            R = None
+
+        self.clcore.phase_factor_qrf(self.q_gpu, self.r_gpu, self.f_gpu, R, self.a_gpu)
+        I = self.a_gpu.get()
+        if ba.get_global('debug') > 0:
+            print(I, I.shape, type(I), I.dtype, np.max(I))
+            print(self.q_gpu.shape)
+        return np.abs(I)**2
