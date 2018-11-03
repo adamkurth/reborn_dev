808f87b7696612752fb41103e9fd5e38b7cd6484
Richard A. Kirian <rkirian@asu.edu>
2018-11-02 22:32:34 -0700

    Updates to detector

--- bornagain/detector.py
+++ bornagain/detector.py
@@ -116,6 +116,7 @@ class PADGeometry(object):
         return (self.n_ss, self.n_fs)
 
     def indices_to_vectors(self, j, i):
+
         r"""
         Convert pixel indices to translation vectors pointing from origin to position on panel.
         The positions need not lie on the actual panel; this assums an infinite plane.
@@ -172,6 +173,15 @@ class PADGeometry(object):
         """
 
         return (2 * np.pi / wavelength) * self.ds_vecs(beam_vec=beam_vec)
+
+    def q_mags(self, beam_vec, wavelength):
+        r"""
+        Calculate scattering vector magnitudes:
+
+        Returns: numpy array
+        """
+
+        return vec_mag(self.q_vecs(beam_vec=beam_vec, wavelength=wavelength))
 
     def solid_angles2(self):
         """
@@ -718,3 +728,92 @@ class IcosphereGeometry(object):
 
         return verts, faces, face_centers
 
+
+class RadialProfiler(object):
+
+    r"""
+    Helper class to create radial profiles.
+    """
+
+    def __init__(self):
+
+        self.n_bins = None
+        self.bins = None
+        self.bin_size = None
+        self.bin_indices = None
+        self.q_mags = None
+        self.mask = None
+        self.counts = None
+        self.q_range = None
+        self.counts_non_zero = None
+
+    def make_plan(self, q_mags, mask=None, n_bins=100, q_range=None):
+        r"""
+        Setup the binning indices for the creation of radial profiles.
+
+        Arguments:
+            q_mags (numpy array) :
+                Scattering vector magnitudes.
+            mask (numpy array) :
+                Pixel mask.  Should be ones and zeros, where one means "good" and zero means "bad".
+            n_bins (int) :
+                Number of bins.
+            q_range (list-like) :
+                The minimum and maximum of the scattering vector magnitudes.  The bin size will be equal to
+                (max_q - min_q) / n_bins
+        """
+
+        q_mags = q_mags.ravel()
+
+        if q_range is None:
+            min_q = np.min(q_mags)
+            max_q = np.max(q_mags)
+            q_range = np.array([min_q, max_q])
+        else:
+            q_range = q_range.copy()
+            min_q = q_range[0]
+            max_q = q_range[1]
+
+        bin_size = (max_q - min_q) / float(n_bins)
+        bins = (np.arange(0, n_bins) + 0.5) * bin_size + min_q
+        bin_indices = np.int64(np.floor((q_mags - min_q) / bin_size))
+        bin_indices[bin_indices < 0] = 0
+        bin_indices[bin_indices >= n_bins] = n_bins - 1
+        if mask is None:
+            mask = np.ones([len(bin_indices)])
+        else:
+            mask = mask.copy().ravel()
+        # print(bin_indices.shape, mask.shape, n_bins)
+        counts = np.bincount(bin_indices, mask, n_bins)
+        counts_non_zero = counts > 0
+
+        self.n_bins = n_bins
+        self.bins = bins
+        self.bin_size = bin_size
+        self.bin_indices = bin_indices
+        self.q_mags = q_mags
+        self.mask = mask
+        self.counts = counts
+        self.q_range = q_range
+        self.counts_non_zero = counts_non_zero
+
+    def get_profile(self, data, average=True):
+        r"""
+        Create a radial profile for a particular dataframe.
+
+        Arguments:
+            data (numpy array) :
+                Intensity data.
+            average (bool) :
+                If true, divide the sum in each bin by the counts, else return the sum.  Default: True.
+
+        Returns:
+            profile (numpy array) :
+                The requested radial profile.
+        """
+
+        profile = np.bincount(self.bin_indices, data.ravel() * self.mask, self.n_bins)
+        if average:
+            profile.flat[self.counts_non_zero] /= self.counts.flat[self.counts_non_zero]
+
+        return profile
--- test/test_detector.py
+++ test/test_detector.py
@@ -4,19 +4,11 @@ import sys
 
 sys.path.append('..')
 import bornagain as ba
+from bornagain import detector
 import numpy as np
 
 
-def test_PADGeometry():
-
-    # TODO: actually test something...
-
-    pad = ba.detector.PADGeometry()
-    assert(pad.t_vec is None)
-
-def test_PADAssembler():
-
-    # TODO: check that layout is actually correct.
+def make_pad_list():
 
     pad_geom = []
     pad = ba.detector.PADGeometry()
@@ -34,6 +26,21 @@ def test_PADAssembler():
     pad.n_ss = 150
     pad_geom.append(pad)
 
+    return pad_geom
+
+def test_PADGeometry():
+
+    # TODO: actually test something...
+
+    pad = ba.detector.PADGeometry()
+    assert(pad.t_vec is None)
+
+def test_PADAssembler():
+
+    # TODO: check that layout is actually correct.
+
+    pad_geom = make_pad_list()
+
     assembler = ba.detector.PADAssembler(pad_geom)
     dat = [p.ones() for p in pad_geom]
 
@@ -42,3 +49,21 @@ def test_PADAssembler():
     assert(np.min(ass) == 0)
     assert(np.max(ass) == 1)
 
+
+def test_radial_profiler():
+
+    pad_geom = make_pad_list()
+
+    beam = [0, 0, 1]
+    wav = 1.5e-10
+
+    q_mags = np.ravel([p.q_mags(beam_vec=beam, wavelength=wav) for p in pad_geom])
+
+    rad = detector.RadialProfiler()
+    rad.make_plan(q_mags, mask=None, n_bins=100, q_range=None)
+
+    data = np.ravel([np.random.rand(p.n_ss, p.n_fs) for p in pad_geom])
+
+    prof = rad.get_profile(data, average=True)
+    assert(np.max(prof) <= 1)
+    assert(np.min(prof) >= 0)
