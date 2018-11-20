a85d51eccb2d9c089b449d312edf66c639d17e4f
Richard A. Kirian <rkirian@asu.edu>
2018-11-20 16:07:09 -0700

    peaker updates

--- bornagain/analysis/peaks.f90
+++ bornagain/analysis/peaks.f90
@@ -68,13 +68,12 @@ subroutine peak_snr_filter(data, a, b, c, mask, local_max_only, snr, signal)
 end subroutine peak_snr_filter
 
 
+module peaker
+   implicit none
+!   integer, private, parameter :: 4=selected_int_kind(9)
+!   integer, private, parameter :: 8=selected_real_kind(15,9)
 
-!module peaker
-!   implicit none
-!!   integer, private, parameter :: i4=selected_int_kind(9)
-!!   integer, private, parameter :: r8=selected_real_kind(15,9)
-!
-!contains
+contains
    subroutine squarediff(p,npx,npy,nosx,nosy,nisx,nisy)
 !
 ! p(npx,npy) input array, overwritten to give output array
@@ -82,11 +81,11 @@ end subroutine peak_snr_filter
 ! not contained in inner square from i-nisx to i+nisx, j-nisy to j+nisy
 !
       real(kind=8), intent(inout) :: p(npx,npy)
-      integer(kind=4), intent(in) :: nosx,nosy,nisx,nisy
+      integer(kind=4), intent(in) :: npx,npy,nosx,nosy,nisx,nisy
       real(kind=8) :: cumx(0:npx,npy),cum1y(npx,0:npy),cum2y(npx,0:npy)
       real(kind=8) :: sqox(npx,npy),sqix(npx,npy)
       integer(kind=4) :: ix,iy
-      cumx(0,:)=0.0
+      cumx(0,:)=0.0_8
       do ix=1,npx
          cumx(ix,:)=cumx(ix-1,:)+p(ix,:)
       enddo
@@ -94,19 +93,20 @@ end subroutine peak_snr_filter
          sqix(ix,:)=cumx(min(npx,ix+nisx),:)-cumx(max(0,ix-nisx-1),:)
          sqox(ix,:)=cumx(min(npx,ix+nosx),:)-cumx(max(0,ix-nosx-1),:)
       enddo
-      cum1y(:,1)=0.0
-      cum2y(:,1)=0.0
+      cum1y(:,1)=0.0_8
+      cum2y(:,1)=0.0_8
       do iy=1,npy
-         cum1y(:,iy)=cum1y(:,iy-1)+sqix(:,iy)
-         cum2y(:,iy)=cum2y(:,iy-1)+sqox(:,iy)
+         cum1y(:,iy)=cum1y(:,iy-1)+sqix(1:npx,iy)
+         cum2y(:,iy)=cum2y(:,iy-1)+sqox(1:npx,iy)
       enddo
       do iy=1,npy
          p(:,iy)=cum2y(:,min(npy,iy+nosy))-cum2y(:,max(0,iy-nosy-1)) &
             -cum1y(:,min(npy,iy+nisy))+cum1y(:,max(0,iy-nisy-1))
       enddo
+
    end subroutine squarediff
 
-!end module peaker
+end module peaker
 
 
 
--- test/test_analysis.py
+++ test/test_analysis.py
@@ -1,11 +1,11 @@
 import sys
 sys.path.append('..')
 import numpy as np
-from bornagain.analysis.peaks_f import peak_snr_filter, squarediff
+from bornagain.analysis import peaks_f
 
 
 def test_peak_snr_filter():
-    # input
+
     nf = 10
     ns = 8
     data = np.random.random((ns, nf)).copy('f')
@@ -22,19 +22,17 @@ def test_peak_snr_filter():
     snr = np.zeros_like(data, order='f')
     signal = np.zeros_like(data, order='f')
 
-    peak_snr_filter(data, a, b, c, mask, local_max_only, snr, signal)
+    peaks_f.peak_snr_filter(data, a, b, c, mask, local_max_only, snr, signal)
 
     assert(snr[4, 6] == 0)
 
 
-def test_peaker():
+def test_fortran():
 
     npx = 100
     npy = 200
     p = np.random.rand(npx, npy)
-    p0 = p.copy('f')
+    p0 = np.asfortranarray(p) #.copy('f')
     nin = 20
     nout = 30
-    squarediff(p0, npx, npy, nout, nout, nin, nin)
-
-
+    peaks_f.peaker.squarediff(p0, nout, nout, nin, nin)
