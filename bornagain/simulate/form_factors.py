0aa34b81b26f5c1900c2e3980013148508dc86f3
Richard A. Kirian <rkirian@asu.edu>
2018-12-12 10:58:58 -0700

    A bit of documentation on the sphere example for Jonah

--- bornagain/simulate/form_factors.py
+++ bornagain/simulate/form_factors.py
@@ -9,7 +9,8 @@ def sphere_form_factor(radius, q_mags, check_divide_by_zero=True):
 
     Form factor for a sphere of given radius, at given q magnitudes.  Assumes the scattering density is unity inside.
 
-    Formula from Table A.1 in Guinier "X-ray diffraction in crystals, imperfect crystals, and amorphous bodies"
+    Formula from Table A.1 in Guinier "X-ray diffraction in crystals, imperfect crystals, and amorphous bodies".  Rick
+    confirms there are no approximations in this formula, and radius is correct (no it's not diameter by mistake).
 
     Args:
         radius (float): In SI units of course
@@ -20,8 +21,8 @@ def sphere_form_factor(radius, q_mags, check_divide_by_zero=True):
 
     """
 
-    arg = q_mags*radius
-    amp = 4*np.pi*radius**3*(np.sin(arg)-arg*np.cos(arg))/arg**3
+    qR = q_mags*radius
+    amp = 4*np.pi*radius**3*(np.sin(qR)-qR*np.cos(qR))/qR**3
     if check_divide_by_zero is True:
         if any(q_mags == 0):
             w = np.where(q_mags == 0)
---
+++ examples/simulations/spheres.py
@@ -0,0 +1,89 @@
+import sys
+
+sys.path.append('../..')
+
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+import bornagain
+from bornagain import simulate
+from bornagain.simulate.clcore import ClCore
+from bornagain.simulate.form_factors import sphere_form_factor
+
+
+# We simulate diffraction from an ensemble of spheres.  Specifically, we have doublets of spheres sitting in a plane
+# tilted 45 deg. with respect to the x-ray beam.  The real-space density is:
+#    f_tot(r) = Sum_n f_sph(r - s_n)
+# where s_n is the position of the n_th sphere.  The Fourier-space density is:
+#    F_tot(q) = Sum_n F_sph(q) exp(i q.s_n)
+# Notably, the form factor of the sphere, F_sph(q), can come out of the sum:
+#    F_tot(q) = F_sph(q) [ Sum_n exp(i q.s_n) ]
+# We simulate the form factor of the sphere once, and do the sum over the position phasors separately.
+#
+# Everything in SI
+#
+# We use a common convention at x-ray facilities:
+#     - Beam is along the "z" axis (i.e. third vector compoment)
+#     - The "y" component is "up"
+#     - The "x" compoment completes the right-handed coordinate system
+#     - Usually beam polarization from an undulator is in the horizontal, which would be "x"
+
+# This prepares the GPU simulation engine.  It computes this sum: Sum_n f_n exp(i q.r_n)   (really fast -- overkill...)
+sim = simulate.clcore.ClCore()
+
+# Generic container for an x-ray beam.  Holds beam direction, polarization, wavelength, etc.
+beam = bornagain.source.Beam(wavelength=13.49e-9, beam_vec=[0, 0, 1])
+
+# Generic container for an pixel-array detector
+detector = bornagain.detector.PADGeometry(n_pixels=2048, pixel_size=13.5e-6, distance=0.07)
+
+# Now we construct the arrangement of spheres:
+radius = 0.5e-6
+pos = np.array([[-5, -5, 0], [-5, 4, 0], [2, 0, 0]])*1e-6
+pos = np.concatenate([pos, pos], axis=0)
+sep = radius*1.6 # guess the sphere separation
+pos[0:3, 2] = -sep
+pos[3:6, 2] = +sep
+angle = np.pi/4
+rot = np.array([[1, 0, 0], [0, np.cos(angle), -np.sin(angle)], [0, np.sin(angle), np.cos(angle)]])
+pos = bornagain.utils.rotate(rot, pos)
+
+# Display the arrangement of spheres:
+if False:
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111, projection='3d')
+    p = pos*1e6
+    ax.scatter(p[:, 0], p[:, 1], p[:, 2], c='k', marker='o') # matplotlib can't get basic 3d axis scaling right...
+    ax.set_xlabel('x (microns)')
+    ax.set_ylabel('y (microns)')
+    ax.set_zlabel('z (microns)')
+    plt.show()
+
+# Compute the scattering amplitudes:
+scat = sim.phase_factor_qrf(q=detector.q_vecs(beam=beam), r=pos, f=np.ones([pos.shape[0]]))
+scat *= simulate.form_factors.sphere_form_factor(radius=radius, q_mags=detector.q_mags(beam=beam))
+scat = np.abs(scat)**2
+scat /= np.max(scat) # Normalize for display purposes
+
+# Mask the direct beam
+beam_mask = np.ones_like(scat)
+beam_mask[detector.scattering_angles(beam=beam) < 0.01] = 0
+
+# The simulation engine doesn't operate on "2D" arrays, so we reshape at the end:
+scat = detector.reshape(scat)
+beam_mask = detector.reshape(beam_mask)
+
+# Display the diffraction intensities:
+if True:
+    cmap = 'CMRmap'
+    fig = plt.figure(2)
+    ax1 = fig.add_subplot(121)
+    dispim1 = scat
+    dispim1 *= beam_mask
+    dispim1 = np.log(dispim1 + 1e-7)
+    ax1.imshow(dispim1, cmap=plt.get_cmap(cmap))
+    dispim2 = np.fft.fftshift(np.abs(np.fft.fftn(scat)))
+    ax2 = fig.add_subplot(122)
+    ax2.imshow(dispim2, cmap=plt.get_cmap(cmap))
+    plt.show()
