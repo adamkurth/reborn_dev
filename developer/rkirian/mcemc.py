3970909e54d6d88a5cab91a1ca195041675ff79d
Richard A. Kirian <rkirian@asu.edu>
2019-04-08 15:24:55 -0700

    mcemc (broken)

--- developer/rkirian/mcemc.py
+++ developer/rkirian/mcemc.py
@@ -35,8 +35,8 @@ a_out_dev = clcore.to_device(dtype=clcore.complex_t, shape=pad.shape())
 clcore.phase_factor_mesh(mol.coordinates, f, N=mesh_size, q_min=-qmax, q_max=qmax, a=a_map_dev)
 
 n_patterns = 1000
-n_orientations = 1000
-skip = 20
+n_orientations = 1
+skip = 0
 
 corner = np.array([-qmax]*3, dtype=real_t)
 deltas = np.array([((qmax - -qmax) / (mesh_size - 1))] * 3, dtype=real_t)
@@ -44,24 +44,34 @@ model = np.zeros(shape=[mesh_size] * 3, dtype=real_t)
 weights = np.zeros(shape=[mesh_size] * 3, dtype=real_t)
 vec0 = q_vecs.astype(real_t).copy()
 
-shotim = pg.image()
+live_update = False
+if live_update:
+    shotim = pg.image(title='Single shot')
+    modelim = pg.image(title='Model update')
+
+# outer loop over model updates to be added
 
 for pat in range(n_patterns):
     true_rot = random_rotation().astype(real_t)
     intensity = sim.generate_pattern(rotation=true_rot, poisson=True).astype(real_t)*mask
-    shotim.setImage(intensity)
-    pg.QtGui.QApplication.processEvents()
+    if live_update:
+        shotim.setImage(intensity)
+        w = np.where(weights.flat > 0)
+        if len(w[0]) == 1:
+            dmodel = np.zeros_like(model)
+            dmodel.flat[w] = model.flat[w]/weights.flat[w]
+            modelim.setImage(dmodel[int(np.floor(mesh_size[0]/2)), :, :])
+        pg.QtGui.QApplication.processEvents()
     w = np.where(intensity.flat > 0)[0]
     intensity_nonzero = intensity.flat[w]
     q_vecs_nonzero = q_vecs[w, :]
     prev_prob = -1e100
     prev_rot = 0
     for orient in range(n_orientations):
-        rot = random_rotation().astype(real_t)
+        rot = true_rot #random_rotation().astype(real_t)
         model_slice = sim.generate_pattern(rotation=rot, poisson=False).astype(real_t)
         model_slice_nonzero = model_slice.flat[w]
-        prob = np.sum(-model_slice_nonzero+intensity_nonzero*log(model_slice_nonzero)
-                          -log(factorial(intensity_nonzero)))
+        prob = np.sum(-model_slice_nonzero+intensity_nonzero*log(model_slice_nonzero)-log(factorial(intensity_nonzero)))
         a = np.random.rand(1)
         if a < exp(prob - prev_prob):
             accept = True
@@ -83,11 +93,11 @@ for pat in range(n_patterns):
             trilinear_insertion(model, weights, rotq, intensity_nonzero, corner, deltas)
 
 
-if 0:
+if 1:
     w = np.where(weights.flat > 0)
     dmodel = np.zeros_like(model)
     dmodel.flat[w] = model.flat[w]/weights.flat[w]
     pg.image(np.log(intensity*mask + 1), title='Single shot')
-    pg.image(np.log(dmodel+1))
-    pg.image(weights)
+    pg.image(np.log(dmodel+1), title='Final model')
+    pg.image(weights, title='Weights')
     pg.QtGui.QApplication.exec_()
