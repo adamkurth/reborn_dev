af57417cf752a592cbb32b37783f5e0e5b742557
Joe Chen <jpchen1@mainex1.asu.edu>
2019-06-20 23:20:11 -0700

    Updated streamfile tester.

--- developer/jchen/notest_streamfilegetter.py
+++ developer/jchen/notest_streamfilegetter.py
@@ -8,11 +8,11 @@ streamfile_path = '../../examples/data/crystfel/streamfiles/r0123-KY-sorted_modi
 
 c = getters.StreamfileFrameGetter(streamfile_path)
 
-print(c.n_frames)
-print(c.streamfile_name)
-print(c.get_frame(0))
+# print(c.n_frames)
+# print(c.streamfile_name)
+# print(c.get_frame(1))
 
-a = c.get_frame(0)
+a = c.get_frame(4)
 print(a['A_matrix'])
 print(a['cxiFilepath'])
 print(a['cxiFileFrameNumber'])
