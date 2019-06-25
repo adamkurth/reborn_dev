Finally implemented the proper weights calculation in trilinear insert. Also added an additional test and fixed one test.

bornagain/utils.py
@@ -423,11 +423,10 @@ else:
                     d_max = d
         return np.sqrt(d_max)
 
-def trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask=None):
+def trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask=None):
     r""""
     Trilinear insertion on a regular grid with arbitrary sample points.
     The boundary is defined as [x_min-0.5, x_max+0.5).
-    N_pattern specifies the number of patterns to insert so that we can divide by it to get the average.
 
     Note 1: All input arrays should be C contiguous.
     Note 2: This code will break if you put a 1 in any of the N_bin entries.
@@ -439,8 +438,7 @@ def trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask=
         x_max     : An array with the three values corresponding to the largest data grid center points.
         N_bin     : An array with the three values corresponding to the number of bins in each direction.
         mask      : An array with the N values specifying which data points to ignore. Zero means ignore.
-        N_pattern : Number of patterns that have been inserted. So that we can average the resultant 3D volume by it. The terminology is due to the fact that we want to use this function to merge Ewald slices into a 3D volume.
-
+    
     Returns:
         A 3D numpy array with trilinearly inserted values.
     """
@@ -480,17 +478,27 @@ def trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask=
     c3 = x_min - 0.5 + epsilon
 
     dataout = np.zeros(N_bin+2, dtype=np.double, order='C')
+    weightout = np.zeros(N_bin+2, dtype=np.double, order='C')
+
     dataout = np.asfortranarray(dataout)
+    weightout = np.asfortranarray(weightout)
     
     fortran.interpolations_f.trilinear_insert(data_coord, data_val, x_min, mask, N_data, \
                                               Delta_x, one_over_bin_volume, c1, c2, c3,  \
-                                              dataout)
+                                              dataout, weightout)
 
     # Keep only the inner array - get rid of the boundary padding.
     dataout = dataout[1:N_bin[0]+1, 1:N_bin[1]+1, 1:N_bin[2]+1]
+    weightout = weightout[1:N_bin[0]+1, 1:N_bin[1]+1, 1:N_bin[2]+1]
 
-    # Divide by the total number of patterns inserted to get the average.
-    dataout /= N_pattern
+    # Calculate the mean value inserted into the array by dividing dataout by weightout.
+    # For locations where weightout is zero, dataout should also be zero (because no values were inserted),
+    # deal with this case by setting weightout to 1.
+    assert np.sum(dataout[weightout == 0]) == 0
+
+    weightout[weightout == 0] = 1
+
+    dataout /= weightout
 
     return dataout
 

test/test_interpolations.py
@@ -1,10 +1,13 @@
 from bornagain import utils
 import numpy as np
 
+# The usage of N_data and N_pattern in some of the tests below can be interpreted as:
+# N_data    : Number of pixels in a detector
+# N_pattern : Number of patterns measured from that detector
+# And the action of the trilinear_insert() is to insert the values measured by the detector
+# into a 3D regularly grided array.
 
 def test_1():
-    N_pattern = 1
-    
     data_coord = np.array([[0.1, 0, 0]])
     data_val = np.array([1])
     mask = np.ones(len(data_val))
@@ -25,13 +28,11 @@ def test_1():
             [ 0. ,  0.1,  0. ],
             [ 0. ,  0. ,  0. ]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-9
 
 
 def test_2():
-    N_pattern = 1
-
     data_coord = np.array([[0.5, 0.5, 0.5]])
     data_val = np.array([1])
     mask = np.ones(len(data_val))
@@ -52,13 +53,11 @@ def test_2():
             [ 0.   ,  0.125,  0.125],
             [ 0.   ,  0.125,  0.125]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-9
 
 
 def test_3():
-    N_pattern = 1
-
     data_coord = np.array([[-1.0, -1.0, -1.0]])
     data_val = np.array([1])
     mask = np.ones(len(data_val))
@@ -79,13 +78,11 @@ def test_3():
             [ 0.,  0.,  0.],
             [ 0.,  0.,  0.]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-9
 
 
 def test_4():
-    N_pattern = 1
-
     data_coord = np.array([[1.0, 1.0, 1.0]])
     data_val = np.array([1])
     mask = np.ones(len(data_val))
@@ -106,13 +103,11 @@ def test_4():
             [ 0.,  0.,  0.],
             [ 0.,  0.,  1.]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-9
 
 
 def test_5():
-    N_pattern = 1
-
     data_coord = np.array([[-0.5, -0.5, -0.5]])
     data_val = np.array([1])
     mask = np.ones(len(data_val))
@@ -133,13 +128,11 @@ def test_5():
             [ 0.   ,  0.   ,  0.   ],
             [ 0.   ,  0.   ,  0.   ]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-9
 
 
 def test_6():
-    N_pattern = 1
-
     data_coord = np.array([[0, 0, 0]])
     data_val = np.array([1])
     mask = np.ones(len(data_val))
@@ -160,13 +153,11 @@ def test_6():
             [ 0.,  0.,  0.],
             [ 0.,  0.,  0.]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-9
 
 
 def test_7():
-    N_pattern = 1
-
     data_coord = np.array([[-1.4999, -1.4999, -1.4999]])
     data_val = np.array([1])
     mask = np.ones(len(data_val))
@@ -188,13 +179,11 @@ def test_7():
             [ 0.        ,  0.        ,  0.        ]]])
 
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-6
 
 
 def test_8():
-    N_pattern = 1
-
     data_coord = np.array([[1.4999, 1.4999, 1.4999]])
     data_val = np.array([1])
     mask = np.ones(len(data_val))
@@ -215,13 +204,11 @@ def test_8():
             [ 0.        ,  0.        ,  0.        ],
             [ 0.        ,  0.        ,  0.12507502]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-6
 
 
 def test_9():
-    N_pattern = 1
-
     data_coord = np.array([[-1.0, 1.0, 1.0]])
     data_val = np.array([20.7])
     mask = np.ones(len(data_val))
@@ -242,13 +229,11 @@ def test_9():
             [  0. ,   0. ,   0. ],
             [  0. ,   0. ,   0. ]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-9
 
 
 def test_10():
-    N_pattern = 1
-
     data_coord = np.array([[-1.3, 0.2, 1.48]])
     data_val = np.array([213.7])
     mask = np.ones(len(data_val))
@@ -269,14 +254,12 @@ def test_10():
             [  0.     ,   0.     ,   0.     ],
             [  0.     ,   0.     ,   0.     ]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-9
 
 
 def test_11():
     N_data = 10
-    N_pattern = 1
-
     data_coord = np.array([[ 0.72128486,  0.22625585,  0.86686662],
            [ 0.25203022,  0.49918326,  0.38627506],
            [ 0.62419278,  0.4668094 ,  0.27941368],
@@ -299,21 +282,19 @@ def test_11():
             [ 0.        ,  0.        ,  0.        ]],
 
            [[ 0.        ,  0.        ,  0.        ],
-            [ 0.        ,  0.76496791,  1.12277353],
-            [ 0.        ,  1.05961945,  0.58409726]],
+            [ 0.        ,  0.076496791,  0.112277353],
+            [ 0.        ,  0.105961945,  0.058409726]],
 
            [[ 0.        ,  0.        ,  0.        ],
-            [ 0.        ,  1.74489971,  1.12437026],
-            [ 0.        ,  2.56917323,  1.03009865]]])
+            [ 0.        ,  0.174489971,  0.112437026],
+            [ 0.        ,  0.256917323,  0.103009865]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-7
 
 
 def test_12():
     N_data = 2
-    N_pattern = 1
-
     data_coord = np.array([[1.0, 1.0, 1.0], [0,0,0]])
     data_val = np.array([1, 1])
     mask = np.ones(N_data)
@@ -338,14 +319,12 @@ def test_12():
             [ 0.  ,  0.  ,  0.  ,  0.  ],
             [ 0.  ,  0.  ,  0.  ,  1.  ]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-9
 
 
 def test_13():
     N_data = 2
-    N_pattern = 1
-
     data_coord = np.array([[1.0, 1.0, 1.0], [0,0,0]])
     data_val = np.array([1, 10])
     mask = np.ones(N_data)
@@ -372,15 +351,12 @@ def test_13():
             [ 0.   ,  0.   ,  0.   ,  0.   ],
             [ 0.   ,  0.   ,  0.   ,  0.   ]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
-
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-9
 
 
 def test_14(): # Out of bounds test
     N_data = 2
-    N_pattern = 1
-
     data_coord = np.array([[12.0, 13.0, 14.0], [-10,-10,-20]])
     data_val = np.array([1, 10])
     mask = np.ones(N_data)
@@ -407,7 +383,7 @@ def test_14(): # Out of bounds test
             [ 0.,  0.,  0.,  0.],
             [ 0.,  0.,  0.,  0.]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
     assert np.sum(np.abs(dataout - ans)) < 1e-9
 
 
@@ -442,7 +418,79 @@ def test_15(): # More-than-one-patterns test
             [ 0.   ,  0.   ,  0.   ,  0.   ],
             [ 0.   ,  0.   ,  0.   ,  0.   ]]])
 
-    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, N_pattern, mask)
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
 
     assert np.sum(np.abs(dataout - ans)) < 1e-9
+
+
+def test_16():
+    N_data = 7
+
+    data_coord = np.array([[1.0, 1.0, 1.0], \
+                           [0,0,0], \
+                           [-56.912701, -89.495757,  -69.0889544], \
+                           [7.5068156,  74.737885,  -16.23046],  \
+                           [400.71784,  60.451844,  -33.82541], \
+                           [-23.791852,  70.368846,  0.36841697], \
+                           [-90.728055,  0.40624265,  -0.82445448]])
+    data_val = np.array([ 19.43855069,  0.17395744,  1.306821901,  6.04473423, \
+            22.1702885,   5.80547311,  152.1111933])
+    mask = np.ones(N_data)
+
+    N_bin = np.array([5, 4, 6])
+    x_min = np.array([-100, -99, -71])
+    x_max = np.array([200, 300, -2])
+
+    ans = np.array([[[ 0.44481935,  0.07150083,  0.        ,  0.        ,  0.        ,
+          0.        ],
+        [ 0.03423333,  0.00550271,  0.        ,  0.        ,  0.        ,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ]],
+
+       [[ 0.60057795,  0.09653766,  0.        ,  0.        ,  0.        ,
+          0.        ],
+        [ 0.04622053,  0.00742955,  0.        ,  0.07410725,  2.30167695,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.03272169,  1.01629419,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ]],
+
+       [[ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.05669123,  1.76075739,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.02503173,  0.7774538 ,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ]],
+
+       [[ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ]],
+
+       [[ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ],
+        [ 0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
+          0.        ]]])
+
+    dataout = utils.trilinear_insert(data_coord, data_val, x_min, x_max, N_bin, mask)
+
+    assert np.sum(np.abs(dataout - ans)) < 1e-7
+
+
+
+
 