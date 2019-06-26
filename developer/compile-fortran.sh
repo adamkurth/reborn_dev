c92a10dcab14c122b36dfcc3bb2811a2c2e7a6ee
Joe Chen <jpchen1@mainex1.asu.edu>
2019-06-26 15:00:56 -0700

    Made peak finder faster with OpenMP.

--- developer/compile-fortran.sh
+++ developer/compile-fortran.sh
@@ -9,7 +9,9 @@ cd $rootdir/bornagain/target
 python -m numpy.f2py -c density.f90 -m density_f --quiet -DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
 
 cd $rootdir/bornagain/analysis
-python -m numpy.f2py -c peaks.f90 -m peaks_f --quiet -DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
+#python -m numpy.f2py -c peaks.f90 -m peaks_f --quiet -DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
+# Use this below if want openmp.
+python -m numpy.f2py -c --f90flags='-fopenmp -O2' peaks.f90 -m peaks_f --quiet -DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
 
 cd $rootdir/bornagain/fortran
 ./compile-fortran.sh
