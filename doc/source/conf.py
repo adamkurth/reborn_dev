aba27d6babf88c2bbb3731881e8db91da3e3bf17
Richard A. Kirian <rkirian@asu.edu>
2019-07-30 18:11:01 -0700

    Switched back to the Read The Docs theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -120,13 +120,14 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-import sphinx_bootstrap_theme
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-html_theme = 'bootstrap'
+# import sphinx_bootstrap_theme
+# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+# html_theme = 'bootstrap'
 # html_theme = 'alabaster'
 # html_theme = 'sphinxdoc'
-# html_theme = "sphinx_rtd_theme"
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
