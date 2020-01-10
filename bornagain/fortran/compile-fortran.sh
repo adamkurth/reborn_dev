bee70e2956d8e699ffc2c0214eaa18e3eca76536
Richard A. Kirian <rkirian@asu.edu>
2020-01-10 15:58:44 -0700

    Update fortran compilation script

--- bornagain/fortran/compile-fortran.sh
+++ bornagain/fortran/compile-fortran.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 f2py="python -m numpy.f2py"
-flags="" #--quiet -DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION"
+flags="-DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION"
 
 ${f2py} -c interpolations.f90 -m interpolations_f ${flags}
 ${f2py} -c wtf.f90 -m wtf_f ${flags}
--- developer/compile-fortran.sh
+++ developer/compile-fortran.sh
@@ -5,8 +5,8 @@ rootdir=$(pwd)
 
 echo root directory: ${rootdir}
 
-cd ${rootdir}/bornagain/target
-python -m numpy.f2py -c density.f90 -m density_f --quiet -DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
+#cd ${rootdir}/bornagain/target
+#python -m numpy.f2py -c density.f90 -m density_f --quiet -DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
 
 cd ${rootdir}/bornagain/fortran
-./compile-fortran.sh
+source compile-fortran.sh
