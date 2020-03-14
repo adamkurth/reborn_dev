3b5766ac41a2caa5ccba6dc157a58c057c65ce63
Richard A. Kirian <rkirian@asu.edu>
2020-03-13 23:15:21 -0700

    Updated scatter.tex note

--- developer/rkirian/misc/scattering_factors.py
+++ developer/rkirian/misc/scattering_factors.py
@@ -12,43 +12,48 @@ eV = const.value('electron volt')
 
 z = 1
 dr = 0.05e-10
-rmax = 100e-10
-r = np.arange(0, rmax, dr) #+ dr*1e-1
+rmax = 10e-10
+r = np.arange(0, rmax, dr)
 n = r.size
 dq = 2 * np.pi / dr / n
 q = np.arange(n) * dq
 f = atoms.hubbel_form_factors(q, z)
 rho = np.imag(np.fft.ifft(q*f))/np.pi/r/dr
+rho[0] = np.sum(q**2*f)*dq/2/np.pi**2
 a0 = 5.2917721e-11
 rho_h = np.exp(-2*r/a0)/np.pi/a0**3  # hydrogen S1
 
 plt.figure()
-plt.loglog(q*1e-10, f)
-plt.xlabel(r'$q = 4\pi \sin(\theta/2)/\lambda \; [\AA{}^{-1}]$')
+plt.loglog(q*1e-10, 16/(a0**2*q**2+4)**2, lw=4, label='True')
+plt.loglog(q*1e-10, f, label='Calc')
+plt.xlabel(r'$q = 4\pi \sin(\theta/2)/\lambda \; [{\rm \AA{}}^{-1}]$')
 plt.ylabel(r'f(q)')
 plt.title(r'Hydrogen Hubbel form factor')
-plt.figure()
-plt.plot(r*1e10, rho_h, label='True')
-plt.plot(r*1e10, rho, label='FT')
-plt.xlim((0, 0.5))
-plt.xlabel(r'$r$ [$\AA{}$]')
-plt.ylabel(r'$\rho(r)$ [$\AA{}^{-3}$]')
+plt.legend()
+f = plt.figure(figsize=plt.figaspect(0.4))
+plt.subplot(121)
+plt.plot(r*1e10, rho_h*1e-30, lw=4, label='True')
+plt.plot(r*1e10, rho*1e-30, label='Calc')
+plt.xlim((0, 2))
+plt.xlabel(r'$r$ [${\rm \AA{}}$]')
+plt.ylabel(r'$\rho(r)$ [${\rm \AA{}}^{-3}$]')
 plt.title(r'Hydrogen S1 electron density')
 plt.legend()
-plt.figure()
-plt.plot(r*1e10, rho_h, label='True')
-plt.plot(r*1e10, rho, label='FT')
-plt.xlabel(r'$r$ [$\AA{}$]')
-plt.ylabel(r'$\rho(r)$ [$\AA{}^{-3}$]')
+plt.subplot(122)
+# plt.plot(r*1e10, rho_h*1e-30, lw=4, label='True')
+# plt.plot(r*1e10, rho*1e-30, label='Calc')
+# plt.xlabel(r'$r$ [${\rm \AA{}}$]')
+# plt.ylabel(r'$\rho(r)$ [${\rm \AA{}}^{-3}$]')
+# plt.title(r'Hydrogen S1 electron density')
+# plt.legend()
+# plt.figure()
+plt.loglog(r*1e10, rho_h*1e-30, lw=4, label='True')
+plt.loglog(r*1e10, rho*1e-30, label='Calc')
+plt.xlabel(r'$r$ [${\rm \AA{}}$]')
+plt.ylabel(r'$\rho(r)$ [${\rm \AA{}}^{-3}$]')
 plt.title(r'Hydrogen S1 electron density')
 plt.legend()
-plt.figure()
-plt.loglog(r*1e10, rho_h, label='True')
-plt.loglog(r*1e10, rho, label='FT')
-plt.xlabel(r'$r$ [$\AA{}$]')
-plt.ylabel(r'$\rho(r)$ [$\AA{}^{-3}$]')
-plt.title(r'Hydrogen S1 electron density')
-plt.show()
+f.savefig("../notes/scatter/figures/hydrogen_density_1.pdf", bbox_inches='tight')
 
 # Checking that xraylib and the Henke tables give (nearly) the same results
 
@@ -72,19 +77,16 @@ for i in range(len(E)):
         Fii = xraylib.Fii(z, this_E / eV / 1000)
         f_xraylib[i, j] = FF + Fi - 1j*Fii
 
-f = plt.figure()
+f = plt.figure(figsize=plt.figaspect(0.4))
 plt.subplot(121)
 plt.semilogy(E/eV, np.real(f_henke), 'r-', label='Henke Real Part')
 plt.semilogy(E/eV, np.imag(f_henke), 'r:', label='Henke Imag Part')
 plt.semilogy(E/eV, np.real(f_xraylib[:, 0]), 'b-', label='xraylib Real Part')
 plt.semilogy(E/eV, np.imag(f_xraylib[:, 0]), 'b:', label='xraylib Real Part')
-plt.xlabel('Photon Energy (eV)')
-plt.ylabel('Scattering factor at Q=0')
+plt.xlabel('Photon Energy [eV]')
+plt.ylabel('Scattering factor at q=0')
 plt.title('Atomic Number %d (%s)' % (z, atoms.atomic_numbers_to_symbols([z])))
 plt.legend()
-# plt.show()
-# f.savefig("anamalous_%d.pdf" % (Z,), bbox_inches='tight')
-
 
 q_mags = 1e10*4*np.pi*np.arange(1000)*3/1000
 E = 8000*eV
@@ -95,13 +97,13 @@ f_hubbel_henke = atoms.hubbel_henke_scattering_factors(q_mags, z, E)
 
 plt.subplot(122)
 plt.semilogy(q_mags/1e10, np.abs(f_hubbel), label='Hubbel')
-plt.semilogy(q_mags/1e10, np.abs(f_xraylib), label='Hubbel w/ xraylib anamalous')
+plt.semilogy(q_mags/1e10, np.abs(f_xraylib), label='xraylib')
 plt.semilogy(q_mags/1e10, np.abs(f_henke), label='Henke')
-plt.semilogy(q_mags/1e10, np.abs(f_hubbel_henke), label='Hubbel form factor, Henke dispersion')
+plt.semilogy(q_mags/1e10, np.abs(f_hubbel_henke), label='Hubbel/Henke')
 plt.title('Atomic Number %d (%s)' % (z, atoms.atomic_numbers_to_symbols([z])))
-plt.xlabel(r'$Q = 4\pi \sin(\theta/2)/\lambda$ [\AA{} angstrom]')
-plt.ylabel(r'|$f(Q)$|: $Q$-dependent scattering factor at 8 keV')
+plt.xlabel(r'$q = 4\pi \sin(\theta/2)/\lambda$ [${\rm \AA{}}^{-1}$]')
+plt.ylabel(r'|$f(q)$|: $q$-dependent scattering factor at 8 keV')
 plt.legend()
+f.savefig("../notes/scatter/figures/formfactor_%d.pdf" % (z,), bbox_inches='tight')
 plt.show()
-# f.savefig("formfactor_%d.pdf" % (Z,), bbox_inches='tight')
 
