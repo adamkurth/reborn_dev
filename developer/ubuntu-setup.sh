52ad8456a193e5f481ea84b26920283c4cca3a52
Richard A. Kirian <rkirian@asu.edu>
2020-03-27 12:06:36 -0700

    Trigger tests (print out gfortran executable path)

--- developer/ubuntu-setup.sh
+++ developer/ubuntu-setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 apt-get -qq -y update
-apt-get -qq -y install apt-utils curl libgl1-mesa-glx
+apt-get -qq -y install apt-utils curl gfortran libgl1-mesa-glx
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --output miniconda.sh
 bash miniconda.sh -b -p miniconda
 export PATH=./miniconda/bin:$PATH
 conda update -n base -c defaults conda
 conda env create -f environment.yml
 source activate reborn
+which gfortran
 python setup.py develop
