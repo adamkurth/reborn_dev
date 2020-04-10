0f99119c6d86854d3ab918cdf4a1154a8bf16053
Richard Kirian <rkirian@asu.edu>
2020-04-10 00:15:49 +0000

    Update ubuntu-setup.sh

--- developer/ubuntu-setup.sh
+++ developer/ubuntu-setup.sh
@@ -5,6 +5,9 @@ apt-get -qq -y install apt-utils curl libgl1-mesa-glx
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --output miniconda.sh
 bash miniconda.sh -b -p miniconda
 export PATH=./miniconda/bin:$PATH
+pwd
+ls ./miniconda
+ls ./miniconda/bin
 conda update -n base -c defaults conda
 conda env create -f environment.yml
 source activate reborn
