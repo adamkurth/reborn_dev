63ee9e517733da3895034994ce4c69b8381d900b
Richard Kirian <rkirian@asu.edu>
2020-04-10 00:18:40 +0000

    Update ubuntu-setup.sh

--- developer/ubuntu-setup.sh
+++ developer/ubuntu-setup.sh
@@ -4,6 +4,8 @@ apt-get -qq -y update
 apt-get -qq -y install apt-utils curl libgl1-mesa-glx
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --output miniconda.sh
 bash miniconda.sh -b -p miniconda
+bash miniconda.sh --help
+bash miniconda.sh -h
 export PATH=./miniconda/bin:$PATH
 pwd
 ls ./miniconda
