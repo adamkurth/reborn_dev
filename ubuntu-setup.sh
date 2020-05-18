b44e2c6b7e0627ae3db05f0f3b6e76e27729d676
Richard A. Kirian <rkirian@asu.edu>
2020-05-18 13:00:16 -0700

    Tracking down issue with gitlab runner

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive  # Avoid interactions on gitlab runners
 apt-get -qq -y update
 apt-get -qq -y install apt-utils curl libgl1-mesa-glx
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --output miniconda.sh
 bash miniconda.sh -b -p miniconda
-export PATH=./miniconda/bin:$PATH
+# export PATH=./miniconda/bin:$PATH
+miniconda/bin/conda init bash
 conda update -n base -c defaults conda
 conda env create --name reborn --file environment.yml
 conda activate reborn
