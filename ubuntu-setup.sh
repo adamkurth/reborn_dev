1d3de538de4bb3e83ed51b816d524f251bdf0245
Richard A. Kirian <rkirian@asu.edu>
2020-05-18 10:58:50 -0700

    Tracking down issue with gitlab runner

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -8,7 +8,7 @@ export PATH=~/miniconda/bin:$PATH
 conda update -n base -c defaults conda
 conda env create --name reborn --file environment.yml
 source activate reborn
-pip install --no-deps --editable .
-#python setup.py develop
+#pip install --no-deps --editable .
+python setup.py develop
 echo $PATH
 which pytest
