780ae55821194bb72b45b7fa8dfc31cbc21d9dd5
Richard A. Kirian <rkirian@asu.edu>
2020-05-18 10:55:55 -0700

    Tracking down issue with gitlab runner

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -4,9 +4,11 @@ apt-get -qq -y update
 apt-get -qq -y install apt-utils curl libgl1-mesa-glx
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --output miniconda.sh
 bash miniconda.sh -b -p miniconda
-export PATH=./miniconda/bin:$PATH
+export PATH=~/miniconda/bin:$PATH
 conda update -n base -c defaults conda
-conda env create -f environment.yml
+conda env create --name reborn --file environment.yml
 source activate reborn
 pip install --no-deps --editable .
 #python setup.py develop
+echo $PATH
+which pytest
