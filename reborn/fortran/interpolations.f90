81647b0ea8fd9e920e55bd2f8be8375521fabeff
Richard A. Kirian <rkirian@asu.edu>
2020-05-23 20:10:52 -0700

    Trying to fix fortran compilation errors caused by either numpy, conda, or the gitlab ubuntu image.

--- reborn/fortran/interpolations.f90
+++ reborn/fortran/interpolations.f90
@@ -1,4 +1,10 @@
-! Testing if this dummy program is necessary, as suggested here: https://github.com/numpy/numpy/issues/14222
+! Suddenly, it became necessary to have this main program in order for these to compile with f2py.  Nothing in the
+! python or fortran code changed in reborn.  Not sure if this is due to numpy or conda, but the failure began on
+! May 23, 2020 when compiling via the gitlab runner.  This is possibly helpful:
+!
+!  https://github.com/numpy/numpy/issues/14222
+!
+! I wonder if the main program will now break compilation on other systems.....
 PROGRAM MAIN
 PRINT *,'Hello world'
 END PROGRAM MAIN
