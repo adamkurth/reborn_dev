6e37fcde7df8f8c07a91c651cf094c6dbd9885e7
Richard A. Kirian <rkirian@asu.edu>
2020-06-01 16:07:45 -0700

    Are gitlab runners failing due to ubuntu 20.04?  Lets try 18.04...

--- setup.py
+++ setup.py
@@ -19,28 +19,32 @@ ext_modules = list()
 #################################################################################################
 
 f2py_macros = [('NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION', '')]
-# omp_args = {'libraries': ['gomp'], 'extra_compile_args': ['-fopenmp']}
-omp_args = {}
+extra_args = {}  # {'extra_compile_args': ['-static']}
+omp_args = {}  # {'libraries': ['gomp'], 'extra_compile_args': ['-fopenmp']}
 ext_modules.append(Extension(
       name='reborn.fortran.interpolations_f',
       sources=['reborn/fortran/interpolations.f90'],
-      define_macros=f2py_macros
+      define_macros=f2py_macros,
+      **extra_args
       ))
 ext_modules.append(Extension(
       name='reborn.fortran.peaks_f',
       sources=['reborn/fortran/peaks.f90'],
       define_macros=f2py_macros,
+      **extra_args,
       **omp_args
       ))
 ext_modules.append(Extension(
       name='reborn.fortran.wtf_f',
       sources=['reborn/fortran/wtf.f90'],
-      define_macros=f2py_macros
+      define_macros=f2py_macros,
+      **extra_args
       ))
 ext_modules.append(Extension(
       name='reborn.fortran.density_f',
       sources=['reborn/fortran/density.f90'],
-      define_macros=f2py_macros
+      define_macros=f2py_macros,
+      **extra_args
       ))
 
 setup(name='reborn',
