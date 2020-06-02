6ca422f360843743ceae62f5fce323c76511fd84
Richard A. Kirian <rkirian@asu.edu>
2020-06-01 19:36:02 -0700

    Success!  I don't know why, but at least the gitlab runner is working now.

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,15 +1,13 @@
 export DEBIAN_FRONTEND=noninteractive
+export NPY_DISTUTILS_APPEND_FLAGS=1
+export NPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
 apt-get -qq -y update
 apt-get -qq -y install apt-utils curl libgl1-mesa-glx
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --output miniconda.sh
 bash miniconda.sh -b -p miniconda
 export PATH=./miniconda/bin:$PATH
 conda update -n base -c defaults conda
-conda env create --name reborn --file environment.yml
-# conda env update --name reborn --file environment.yml
+conda env create --name reborn
+conda env update --name reborn --file environment.yml
 source activate reborn
-which pip
-export NPY_DISTUTILS_APPEND_FLAGS=1
-export NPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
 pip install --no-deps --editable .
-which pytest
