6d39d14b00e3eb1bb1b4db0a962dfe58316087b2
Richard A. Kirian <rkirian@asu.edu>
2020-06-01 19:40:17 -0700

    Woops...

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -7,7 +7,7 @@ curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --out
 bash miniconda.sh -b -p miniconda
 export PATH=./miniconda/bin:$PATH
 conda update -n base -c defaults conda
-conda env create --name reborn
-conda env update --name reborn --file environment.yml
+conda env create --name reborn --file environment.yml
+# conda env update --name reborn --file environment.yml
 source activate reborn
 pip install --no-deps --editable .
