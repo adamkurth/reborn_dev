6edb1caef9c8de42c932028a0dc4c105339c0783
Richard A. Kirian <rkirian@asu.edu>
2020-07-18 16:55:55 -0700

    Again

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -9,5 +9,5 @@ conda env create --name reborn --file environment.yml
 source activate reborn
 export NPY_DISTUTILS_APPEND_FLAGS=1
 export NPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
-export PYTHONPATH=$(pwd)
+export PYTHONPATH=$(pwd):$PYTHONPATH
 #pip -v install --no-deps --editable .
