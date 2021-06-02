ac49ad883fa9d04fb80a3d59d427ca97af6f35be
Kevin Schmidt <w9cf@arrl.net>
2021-06-02 13:55:07 -0700

    Added test routine for insertion with factor

--- reborn/fortran/density.f90
+++ reborn/fortran/density.f90
@@ -185,6 +185,68 @@ subroutine trilinear_insertion_complex(densities, weights, vectors, vals, corner
     enddo
 end subroutine trilinear_insertion_complex
 
+subroutine trilinear_insertion_factor_real(summedvalues, vectors, vals, corners, deltas,factor2)
+    implicit none
+    real(kind=8), intent(inout) :: summedvalues(:,:,:,:)
+    real(kind=8), intent(in) :: vals(:),factor2
+    real(kind=8) :: val
+    real(kind=8), intent(in) :: vectors(:,:), corners(3), deltas(3)
+    real(kind=8) :: i_f,j_f,k_f,x0,y0,z0,x1,y1,z1
+    real(kind=8) :: f1,f2,f3,f4,f5,f6,f7,f8
+    integer(kind=4) :: i0,j0,k0,i1,j1,k1,ii,nx,ny,nz,nn
+    nn = size(vals, 1)
+    if (size(summedvalues,1).ne.2) then
+       stop 'fix me'
+    endif
+    nx = size(summedvalues, 2)
+    ny = size(summedvalues, 3)
+    nz = size(summedvalues, 4)
+    do ii=1,nn
+        k_f = 1.0 + (vectors(1, ii) - corners(1)) / deltas(1)
+        j_f = 1.0 + (vectors(2, ii) - corners(2)) / deltas(2)
+        i_f = 1.0 + (vectors(3, ii) - corners(3)) / deltas(3)
+        i0 = modulo(floor(i_f) - 1, nz) + 1
+        j0 = modulo(floor(j_f) - 1, ny) + 1
+        k0 = modulo(floor(k_f) - 1, nx) + 1
+        i1 = modulo(i0, nz) + 1
+        j1 = modulo(j0, ny) + 1
+        k1 = modulo(k0, nx) + 1
+        x0 = i_f - floor(i_f)
+        y0 = j_f - floor(j_f)
+        z0 = k_f - floor(k_f)
+        x1 = 1.0 - x0
+        y1 = 1.0 - y0
+        z1 = 1.0 - z0
+        f1 = x1 * y1 * z1
+        f2 = x0 * y1 * z1
+        f3 = x1 * y0 * z1
+        f4 = x1 * y1 * z0
+        f5 = x0 * y1 * z0
+        f6 = x1 * y0 * z0
+        f7 = x0 * y0 * z1
+        f8 = x0 * y0 * z0
+
+        val = vals(ii)
+
+        summedvalues(1, i0, j0, k0) = summedvalues(1, i0, j0, k0) + val * f1
+        summedvalues(2, i0, j0, k0) = summedvalues(2, i0, j0, k0) + factor2 * f1
+        summedvalues(1, i1, j0, k0) = summedvalues(1, i1, j0, k0) + val * f2
+        summedvalues(2, i1, j0, k0) = summedvalues(2, i1, j0, k0) + factor2 * f2
+        summedvalues(1, i0, j1, k0) = summedvalues(1, i1, j0, k0) + val * f3
+        summedvalues(2, i0, j1, k0) = summedvalues(2, i1, j0, k0) + factor2 * f3
+        summedvalues(1, i0, j0, k1) = summedvalues(1, i0, j0, k1) + val * f4
+        summedvalues(2, i0, j0, k1) = summedvalues(2, i0, j0, k1) + factor2 * f4
+        summedvalues(1, i1, j0, k1) = summedvalues(1, i1, j0, k1) + val * f5
+        summedvalues(2, i1, j0, k1) = summedvalues(2, i1, j0, k1) + factor2 * f5
+        summedvalues(1, i0, j1, k1) = summedvalues(1, i0, j1, k1) + val * f6
+        summedvalues(2, i0, j1, k1) = summedvalues(2, i0, j1, k1) + factor2 * f6
+        summedvalues(1, i1, j1, k0) = summedvalues(1, i1, j1, k0) + val * f7
+        summedvalues(2, i1, j1, k0) = summedvalues(2, i1, j1, k0) + factor2 * f7
+        summedvalues(1, i1, j1, k1) = summedvalues(1, i1, j1, k1) + val * f8
+        summedvalues(2, i1, j1, k1) = summedvalues(2, i1, j1, k1) + factor2 * f8
+    enddo
+end subroutine trilinear_insertion_factor_real
+
 
 
 !subroutine build_atomic_scattering_density_map(densities, weights, vectors, vals, corners, deltas, weight)
