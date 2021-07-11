0c1c3055e8deb334a6d3c0ff37b1592f68d52e37
Kevin Schmidt <Kevin.Schmidt@asu.edu>
2021-07-10 22:46:17 -0700

    Changed to rotation input. Made python version a class, with subclasses for Joe equivalent order, and a subclass for a preliminary vkfft version.

--- developer/kschmidt/rotate3d/f90/r3d.py
+++ developer/kschmidt/rotate3d/f90/r3d.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
    import matplotlib.pyplot as plt
    from reborn.utils import rotate3D
    from r3df03py import rotate3dfftw
-   from r3dks import rotate3Dks
+   from r3dks import rotate3D as rotate3Dks
+   from r3dks import rotate3Djoeorder
+   from r3dks import rotate3Dvkfft
+   import scipy
    
    fin = plt.imread('../bw.png').astype(np.float64)
    assert(fin.shape[0] == fin.shape[1])
@@ -18,45 +21,68 @@ if __name__ == "__main__":
    fr[Nadd:Nadd+N0,Nadd:Nadd+N0] = fin
    f3d = np.zeros((N,N,N),dtype=np.complex128)
    f3 = np.zeros((N,N,N),dtype=np.complex128)
-   for i in range(N):
+   for i in range(Nadd,N-Nadd):
       f3d[i,:,:] = fr
       f3[i,:,:] = fr
    
-#   euler = (0.1, 0.0, 0.0)
-#   eulerj = (-0.1, 0.0, 0.0)
-#   euler = (5.1, 0.0, 0.0)
-#   eulerj = (-5.1, 0.0, 0.0)
    euler = np.zeros(3,dtype=np.float64)
    eulerj = np.zeros(3,dtype=np.float64)
    euler[0] = 0.1+np.pi
-   euler[1] = 0.17
+   euler[1] = 0.70
    euler[2] = 0.35
-   eulerj[:] = euler[:]
-   eulerj[0] = -eulerj[0]
-   eulerj[2] = -eulerj[2]
+   eulerj[:] = -euler[:]
+   R = scipy.spatial.transform.Rotation.from_euler('xyx',eulerj)
 
-# For these to be identical, with no negative signs, change joe's code to
-# f_rot[ii, :, :] = np.transpose(rotate2D(np.transpose(f[ii, :, :]), kxfac, xfac, kyfac, yfac, n90_mod_Four))
    t1 = time.time()
    rotate3dfftw(f3.T,euler)
    t2 = time.time()
-   f1 = rotate3D(f3d,eulerj)
-   t3 = time.time()
-   f2 = rotate3Dks(f3d,euler)
-   t4 = time.time()
-   for i in range(N):
+   print("fftw ",t2-t1,"seconds")
+
+   t1 = time.time()
+   f1 = rotate3D(f3d,R)
+   t2 = time.time()
+   print("joe ",t2-t1,"seconds")
+
+   t1 = time.time()
+   r3df = rotate3Dks(f3d)
+   r3df.rotation(R)
+   f2 = r3df.f
+   t2 = time.time()
+   print("kevin pure python plus copies",t2-t1,"seconds")
+
+   t1 = time.time()
+   r3df = rotate3Djoeorder(f3d)
+   r3df.rotation(R)
+   f4 = r3df.f
+   t2 = time.time()
+   print("joe order pure python plus copies",t2-t1,"seconds")
+
+   for i in range(Nadd,N-Nadd):
       f3[i,:,:] = fr
-   t5 = time.time()
+   t1 = time.time()
    rotate3dfftw(f3.T,euler)
-   t6 = time.time()
+   t2 = time.time()
+   print("fftw second call",t2-t1,"seconds")
+
+   t1 = time.time()
+   r3df = rotate3Dvkfft(f3d)
+   t2 = time.time()
+   print("vkfft create instance",t2-t1,"seconds")
+   t1 = time.time()
+   r3df.rotation(R)
+   f5 = r3df.f
+   t2 = time.time()
+   print("vkfft plus copies",t2-t1,"seconds")
    ns=3*N//4
    print("original",f3d[ns,ns,ns])
-   print(f1[ns,ns,ns],f2[ns,ns,ns],f3[ns,ns,ns])
-   print("joe ",t3-t2,"seconds")
-   print("kevin ",t4-t3,"seconds")
-   print("fftw ",t2-t1,"seconds")
-   print("fftw second time ",t6-t5,"seconds")
-   
+   print("joe, kevin, fftw, joeorder, vkfft ")
+   print(f1[ns,ns,ns],f2[ns,ns,ns],f3[ns,ns,ns],f4[ns,ns,ns],f5[ns,ns,ns])
+   print("joe-kevin",np.max(np.abs(f1-f2)))
+   print("joe-fftw",np.max(np.abs(f1-f3)))
+   print("kevin-fftw",np.max(np.abs(f2-f3)))
+   print("kevin-vkfft",np.max(np.abs(f2-f5)))
+   print("joe-joeorder",np.max(np.abs(f1-f4)))
+
    print("joe rotation")
    plt.imshow(np.real(f1[ns,:,:]),cmap=plt.cm.gray)
    plt.show()
@@ -66,3 +92,11 @@ if __name__ == "__main__":
    print("fftw rotation")
    plt.imshow(np.real(f3[ns,:,:]),cmap=plt.cm.gray)
    plt.show()
+   print("vkfft rotation")
+   plt.imshow(np.real(f5[ns,:,:]),cmap=plt.cm.gray)
+   plt.show()
+   print("joeorder rotation")
+   plt.imshow(np.real(f4[ns,:,:]),cmap=plt.cm.gray)
+   plt.show()
+
+
