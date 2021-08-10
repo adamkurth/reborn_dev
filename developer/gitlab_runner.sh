0a4ef6de31eab6b709fb2d97c1a6d9935ee3799a
Richard A. Kirian <rkirian@asu.edu>
2021-08-09 20:20:55 -0700

    Update gitlab runner script

--- developer/gitlab_runner.sh
+++ developer/gitlab_runner.sh
@@ -11,8 +11,16 @@ fi
 # Note that we use the local docker image when available.  If the local docker image is updated, you need to push it
 # to gitlab in order for it to be used by the gitlab servers.  See developer/docker/build_docker.sh .
 cd ..
-gitlab-runner exec docker --docker-pull-policy="if-not-present" tests
+
+if [[ "$1" == "test" ]]; then
+	gitlab-runner exec docker --docker-pull-policy="if-not-present" tests
+	exit
+fi
 
 if [[ "$1" == "doc" ]]; then
   gitlab-runner exec docker --docker-pull-policy="if-not-present" doc
+  exit 
 fi
+
+gitlab-runner exec docker --docker-pull-policy="if-not-present" tests
+gitlab-runner exec docker --docker-pull-policy="if-not-present" doc
