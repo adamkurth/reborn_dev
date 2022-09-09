b1928c5b96b61c257010adddd65535400ae4a5fc
Richard A. Kirian <rkirian@asu.edu>
2022-09-09 07:48:22 -0700

    Cleanup

--- developer/cleanup_docs.sh
+++ developer/cleanup_docs.sh
@@ -7,7 +7,7 @@ fi
 
 docdir=$(pwd)/../doc
 echo cleaning docs
-rm -r $docdir/source/_static/dipole_html
+rm -r $docdir/source/_static/dipole_html &> /dev/null
 rm -r $docdir/build $docdir/source/api $docdir/source/auto_examples &> /dev/null
 latexdir=$docdir/latex
 cd $latexdir/dipole
