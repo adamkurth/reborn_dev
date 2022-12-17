eaf8c39781d4a1675f01c0831095053693ce4994
Richard A. Kirian <rkirian@asu.edu>
2022-12-17 13:54:49 -0800

    Fix filenames in LY59 example

--- developer/examples/lcls/cxily5921/available_runs.sh
+++ developer/examples/lcls/cxily5921/available_runs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ls /reg/d/psdm/cxi/cxily5921/xtc | grep s00-c00 | cut -d '-' -f 2 | sed 's/r//g'
+#ls /reg/d/psdm/cxi/cxily5921/xtc | grep s00-c00 | cut -d '-' -f 2 | sed 's/r//g'
+ls /reg/d/psdm/cxi/cxix53120/xtc | grep s00-c00 | cut -d '-' -f 2 | sed 's/r//g'
 
