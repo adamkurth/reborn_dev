9e28b27d5619dcd44ffdd41de40d8ac4a61e1225
Richard A. Kirian <rkirian@asu.edu>
2023-04-12 10:58:37 -0700

    P172

--- developer/rkirian/examples/lcls/mfxp17218/config.py
+++ developer/rkirian/examples/lcls/mfxp17218/config.py
@@ -2,22 +2,32 @@
 These are all the relevant global configurations for the experiment analysis.
 You can determine info such as pad_id with 'detnames', e.g.
 > detnames exp=mfxp17218:run=356
+Often times the detector stage position can be found with detnames also, e.g.
+> detnames -e exp=mfxp17218:run=356 | grep detector  
 Note that you need to first run setup.sh:
 > source setup.sh
 """
 import copy
+
 config = dict()
-config['experiment_id'] = 'mfxp17218'
-config['results_directory'] = 'results/'
-config['pad_detectors'] = [{'pad_id': 'epix10k2M',
-                            'motions': [0, 0, 2], 
-                            'geometry': None,
-                            'mask': None}]
+config["experiment_id"] = "mfxp17218"
+config["results_directory"] = "results/"
+config["pad_detectors"] = [
+    {
+        "pad_id": "epix10k2M",
+        #'motions': [0, 0, 2],
+        "motions": {"epics_pv": "detector_z"},
+        "geometry": None,
+        "mask": None,
+    }
+]
+
+
 def get_run_config(run_number):
-    """ 
+    """
     Modify the base config dictionary for a particular run (e.g. if geometry or detectors differ
     between runs)
     """
     c = copy.deepcopy(config)
-    c['run_number'] = run_number
+    c["run_number"] = run_number
     return c
