a5654f7248eb2c9f202b798a20dc0d4a697314df
Richard A. Kirian <rkirian@asu.edu>
2023-04-16 23:47:47 -0700

    black

--- reborn/viewers/qtviews/plugins/run_stats.py
+++ reborn/viewers/qtviews/plugins/run_stats.py
@@ -23,11 +23,13 @@ from reborn.viewers.qtviews.padviews import PADView
 cpu_count = multiprocessing.cpu_count()
 
 
-class Plugin():
+class Plugin:
     widget = None
+
     def __init__(self, padview):
         self.widget = Widget(padview)
         self.widget.show()
+
 
 class Widget(qwgt.QWidget):
     stats = None
@@ -37,32 +39,32 @@ class Widget(qwgt.QWidget):
     def __init__(self, padview):
         super().__init__()
         self.padview = padview
-        self.setWindowTitle('Run Stats')
+        self.setWindowTitle("Run Stats")
         self.layout = qwgt.QGridLayout()
         row = 0
         row += 1
-        self.layout.addWidget(qwgt.QLabel('Start Frame'), row, 1)
+        self.layout.addWidget(qwgt.QLabel("Start Frame"), row, 1)
         self.start_frame_spinbox = qwgt.QSpinBox()
         self.start_frame_spinbox.setMinimum(0)
         self.start_frame_spinbox.setMaximum(self.padview.frame_getter.n_frames)
         self.start_frame_spinbox.setValue(0)
         self.layout.addWidget(self.start_frame_spinbox, row, 2)
         row += 1
-        self.layout.addWidget(qwgt.QLabel('Stop Frame'), row, 1)
+        self.layout.addWidget(qwgt.QLabel("Stop Frame"), row, 1)
         self.stop_frame_spinbox = qwgt.QSpinBox()
         self.stop_frame_spinbox.setMinimum(1)
         self.stop_frame_spinbox.setMaximum(self.padview.frame_getter.n_frames)
         self.stop_frame_spinbox.setValue(self.padview.frame_getter.n_frames)
         self.layout.addWidget(self.stop_frame_spinbox, row, 2)
         row += 1
-        self.layout.addWidget(qwgt.QLabel('Step Frame'), row, 1)
+        self.layout.addWidget(qwgt.QLabel("Step Frame"), row, 1)
         self.step_frame_spinbox = qwgt.QSpinBox()
         self.step_frame_spinbox.setMinimum(1)
         self.step_frame_spinbox.setMaximum(self.padview.frame_getter.n_frames)
         self.step_frame_spinbox.setValue(1)
         self.layout.addWidget(self.step_frame_spinbox, row, 2)
         row += 1
-        self.layout.addWidget(qwgt.QLabel('N Processes'), row, 1)
+        self.layout.addWidget(qwgt.QLabel("N Processes"), row, 1)
         self.np_spinbox = qwgt.QSpinBox()
         self.np_spinbox.setMinimum(1)
         self.np_spinbox.setMaximum(cpu_count)
@@ -95,6 +97,7 @@ class Widget(qwgt.QWidget):
         stop = int(self.stop_frame_spinbox.value())
         step = int(self.step_frame_spinbox.value())
         config = dict()
+        # TODO: Get the config options into the GUI
         # config = dict(log_file=tmp+'/runstats/logs/',
         #               checkpoint_interval=250,
         #               checkpoint_file=tmp+'/runstats/checkpoints/',
@@ -102,33 +105,31 @@ class Widget(qwgt.QWidget):
         self.start_button.setEnabled(False)
         self.show_button.setEnabled(False)
         self.stop_button.setEnabled(True)
-        self.worker = Worker(self, framegetter=self.padview.frame_getter, start=start, stop=stop, step=step,
-                             n_processes=n_processes, config=config)
+        self.worker = Worker(
+            self,
+            framegetter=self.padview.frame_getter,
+            start=start,
+            stop=stop,
+            step=step,
+            n_processes=n_processes,
+            config=config,
+        )
         self.worker.start()
-        # while self.worker.processing:
-        #     QtCore.QThread.sleep(1)
-        # self.start_button.setEnabled(True)
-        # self.show_button.setEnabled(True)
-        # self.stop_button.setEnabled(False)
-        # self.timer = QtCore.QTimer(100)
-        # self.timer.timeout.connect(self.padstats_completed)
-        print('Started worker')
-        # self.stats = padstats(framegetter=self.padview.frame_getter, start=start, stop=stop, step=step,
-        #                      n_processes=n_processes, config=config)
+        self.padview.debug("runstats - Started worker")
 
     def padstats_completed(self):
         self.start_button.setEnabled(True)
         self.show_button.setEnabled(True)
         self.stop_button.setEnabled(False)
-        print('Worker is done')
+        self.padview.debug("runstats - Worker is done")
 
     def show_padstats(self):
-        self.padview.debug('show_padstats')
+        self.padview.debug("runstats - show_padstats")
         if self.stats is None:
-            print('No stats to show yet... wait for the job to finish...')
+            self.padview.debug("runstats - No stats to show yet...")
             return
-        self.stats['pad_geometry'] = self.padview.dataframe.get_pad_geometry()
-        self.stats['beam'] = self.padview.dataframe.get_beam()
+        self.stats["pad_geometry"] = self.padview.dataframe.get_pad_geometry()
+        self.stats["beam"] = self.padview.dataframe.get_beam()
         if self.pv is None:
             self.pv = view_padstats(self.stats, start=False, main=False)
         else:
@@ -136,8 +137,8 @@ class Widget(qwgt.QWidget):
 
     def terminate_thread(self):
         if self.worker is not None:
-            print('Attempting to exit thread...')
-            self.worker.exit()
+            self.padview.debug("runstats - Cannot terminate processes...")
+            # self.worker.exit()
 
 
 class Worker(QtCore.QThread):
@@ -147,19 +148,9 @@ class Worker(QtCore.QThread):
         self.args = args
         self.kwargs = kwargs
         self.stats = None
+
     def run(self):
-        # self.parent.start_button.setEnabled(False)
-        # self.parent.show_button.setEnabled(False)
-        # self.parent.stop_button.setEnabled(True)
         self.parent.stats = padstats(*self.args, **self.kwargs)
         self.parent.padstats_completed()
-        # self.parent.start_button.setEnabled(True)
-        # self.parent.show_button.setEnabled(True)
-        # self.parent.stop_button.setEnabled(False)
-        print('DEBUG:pad_stats:Worker: Thread is done')
+        self.parent.padview.debug("runstats - Quitting thread...")
         self.quit()
-        # print('terminated')
-        # self.parent.sig_worker_done.emit()
-        # self.parent.show_padstats()
-        # self.quit()
-        # self.wait()
