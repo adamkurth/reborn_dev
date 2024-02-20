8326888aab5f9285e6169cec735323c7908c3f55
Adam Kurth <amkurth@asu.edu>
2024-02-20 09:51:24 -0700

    Eiger4M.geom and 16M.geom

--- developer/rkirian/projects/cxls/water_background.py
+++ developer/rkirian/projects/cxls/water_background.py
@@ -10,16 +10,17 @@ from reborn.viewers.qtviews import PADView
 from reborn.detector import RadialProfiler
 from scipy import constants as const
 
-from reborn.external import crystfel # crystfel.py interested in load_crystfel_geometry and geometry_dict_to_pad_geometry_list
+from reborn.external import crystfel 
+# crystfel.py interested in load_crystfel_geometry and geometry_dict_to_pad_geometry_list
 
 def save_h5(data, filepath):
-    with h5py.File(filepath, 'w') as f:
-        f.create_dataset('data', data=data)
+    with h5py.File(filepath, 'w') as file:
+        file.create_dataset('entry/data/data', data=data)
+    print(f"Data saved to {filepath} in entry/data/data")
 
-def load_mask(filepath):
-    with h5py.File(filepath, 'r') as f:
-        mask = f['mask_file'][:]
-    return mask
+def apply_water_image(data, water_image):
+    print(f"Applying water image of shape {water_image.shape} to data of shape {data.shape}")
+    return data * water_image
 
 r_e = const.value('classical electron radius')
 eV = const.value('electron volt')
@@ -28,9 +29,9 @@ eV = const.value('electron volt')
 # Configuration (everything in SI units)
 #############################################################
 # detector_shape = [2167, 2167]
-detector_shape = [4371, 4150] # for Eiger4M
+detector_shape = [2162, 2068] # for Eiger4M
 pixel_size = 75e-6
-detector_distance = 0.1  # Sample to detector distance
+detector_distance = 0.3  # Sample to detector distance
 sample_thickness = 100e-6  # Assuming a sheet of liquid of this thickness
 n_shots = 1000  # Number of shots to integrate
 n_photons = 1e7  # Photons per shot
@@ -47,16 +48,21 @@ protein_concentration = 10  #  Concentration of protein (mg/ml, which is same as
 # pad = reborn.detector.PADGeometry(distance=detector_distance, shape=detector_shape, pixel_size=pixel_size)
 
 # added by adam
+
+# relative paths to grab geometry file
 cwd = os.getcwd()
-base_dir_index = cwd.find('reborn_dev')
+base_dir_index = cwd.find('reborn_dev') # or 'reborn'
 if base_dir_index != -1:
     base_dir_path = cwd[:base_dir_index + len('reborn_dev')]
+    water_background_path = os.path.join(base_dir_path, "developer/rkirian/projects/water/adam")
     geom_file_path = os.path.join(base_dir_path, "developer/rkirian/projects/water/adam/Eiger4M.geom")
 else:
     print("Base directory 'reborn_dev' not found in the current working directory")
-    
+
 geom_dictonary = crystfel.load_crystfel_geometry(geom_file_path)
 pad = crystfel.geometry_dict_to_pad_geometry_list(geom_dictonary)[0]
+print(geom_dictonary)
+print(pad)
 
 # end of added by adam
 
@@ -85,6 +91,27 @@ prof = profiler.get_mean_profile(I)
 I = pad.reshape(I)
 I *= mask.astype(int)
 
+# save/write hdf5 file
+water_image_path = os.path.join(water_background_path, 'water_background.h5')
+# save_h5(I, water_image_path) # save the water image
+
+
+
+
+
+# 1. simulate images (not water background) with specific protein and first detector distance 
+# 2. keep these images on agave, but apply the water background to these images and copy them to a new directory
+#    - make sure there are empty images (only water in the image) and images with peaks
+# 3. train the neural network on the images with the water background and normal images
+# 4. repeat steps 1-3 for the second detector distance
+
+
+# feed this through the simulated data in agave
+# of the simulated images(1) add the water background image (2) to produce a new image
+# these are the images that we will train the neural network on
+# empty image (only water in the image), is bad 
+# with peaks, is good
+# train on two different detector distances .
 
 
 if 'noplots' not in sys.argv:
--- developer/rkirian/projects/water/adam/Eiger4M.geom
+++ developer/rkirian/projects/water/adam/Eiger4M.geom
@@ -1,16 +1,16 @@
-; 08/09/18 zatsepin 
-; FMX @ NSLS II August 2018
-; Eiger 16M, 12 keV. 
+;Sabine -Eiger for CXLS simulations
+
+; EIGER 4M (2162, 2068) 
 
 ; Camera length (in m) and photon energy (eV)
-clen = 0.135
-photon_energy = 5000
+clen = 0.10 
+photon_energy = 7000
 
 ; adu_per_photon needs a relatively recent CrystFEL version.  If your version is
 ; older, change it to adu_per_eV and set it to one over the photon energy in eV
 
 adu_per_photon = 1
-res = 13333.3   ; 75 micron pixel size
+res = 13333.33   ; 75 micron pixel size
 
 ; These lines describe the data layout for the Eiger native multi-event files
 dim0 = %
@@ -20,121 +20,21 @@ data = /entry/data/data
 
 
 ; Mask out strips between panels
-bad_v0/min_fs = 1030
-bad_v0/max_fs = 1039
-bad_v0/min_ss = 0
-bad_v0/max_ss = 4370
-
-bad_v1/min_fs = 2070
-bad_v1/max_fs = 2079
-bad_v1/min_ss = 0
-bad_v1/max_ss = 4370
-
-bad_v2/min_fs = 3110
-bad_v2/max_fs = 3119 
-bad_v2/min_ss = 0
-bad_v2/max_ss = 4370
-
-bad_h0/min_fs = 0
-bad_h0/max_fs = 4149
-bad_h0/min_ss = 514
-bad_h0/max_ss = 550
-
-bad_h1/min_fs = 0
-bad_h1/max_fs = 4149
-bad_h1/min_ss = 1065
-bad_h1/max_ss = 1101
-
-bad_h2/min_fs = 0
-bad_h2/max_fs = 4149
-bad_h2/min_ss = 1616
-bad_h2/max_ss = 1652
-
-bad_h3/min_fs = 0
-bad_h3/max_fs = 4149
-bad_h3/min_ss = 2167
-bad_h3/max_ss = 2203
-
-bad_h4/min_fs = 0
-bad_h4/max_fs = 4149
-bad_h4/min_ss = 2718
-bad_h4/max_ss = 2754
-
-bad_h5/min_fs = 0
-bad_h5/max_fs = 4149
-bad_h5/min_ss = 3269
-bad_h5/max_ss = 3305
-
-bad_h6/min_fs = 0
-bad_h6/max_fs = 4149
-bad_h6/min_ss = 3820
-bad_h6/max_ss = 3856
-
-bad_bs/min_fs = 1880
-bad_bs/max_fs = 2100
-bad_bs/min_ss = 2200
-bad_bs/max_ss = 2350
-
-bad_bp1/min_fs = 1963
-bad_bp1/max_fs = 1964
-bad_bp1/min_ss = 600
-bad_bp1/max_ss = 601
-
-bad_bp2/min_fs = 3931
-bad_bp2/max_fs = 3932  
-bad_bp2/min_ss = 1201   
-bad_bp2/max_ss = 1202
-
-bad_bp3/min_fs = 1606
-bad_bp3/max_fs = 1607
-bad_bp3/min_ss = 1740
-bad_bp3/max_ss = 1741
-
-bad_bp4/min_fs = 1180
-bad_bp4/max_fs = 1181
-bad_bp4/min_ss = 1884
-bad_bp4/max_ss = 1885
-
-bad_bp5/min_fs = 1542
-bad_bp5/max_fs = 1543
-bad_bp5/min_ss = 2664
-bad_bp5/max_ss = 2665
-
-bad_bp6/min_fs = 1934
-bad_bp6/max_fs = 1935
-bad_bp6/min_ss = 2889
-bad_bp6/max_ss = 2890
-
-bad_bp7/min_fs = 1656
-bad_bp7/max_fs = 1657
-bad_bp7/min_ss = 3239
-bad_bp7/max_ss = 3240
-
-bad_bp8/min_fs = 1200
-bad_bp8/max_fs = 1201
-bad_bp8/min_ss = 3382
-bad_bp8/max_ss = 3383
-
-bad_bp9/min_fs = 1692
-bad_bp9/max_fs = 1693
-bad_bp9/min_ss = 4085
-bad_bp9/max_ss = 4086
-
 
 ; Uncomment these lines if you have a separate bad pixel map (recommended!)
-mask_file = mask_comb_V2.h5
-mask = /data/data
-mask_good = 1
-mask_bad = 0
+; mask_file = mask-march.h5
+; mask = /data/data
+; mask_good = 1
+; mask_bad = 0
 
 ; corner_{x,y} set the position of the corner of the detector (in pixels)
 ; relative to the beam
 panel0/min_fs = 0
-panel0/max_fs = 4149
+panel0/max_fs = 2068
 panel0/min_ss = 0
-panel0/max_ss = 4370
-panel0/corner_x = -2000.919900
-panel0/corner_y = -2249.374056
+panel0/max_ss = 2162
+panel0/corner_x = -1034
+panel0/corner_y = -1080
 panel0/fs = +1.000000x +0.000000y
 panel0/ss = +0.000000x +1.000000y
 
